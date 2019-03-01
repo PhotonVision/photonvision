107b0dfb27c81e691f51cb679e2602a20d5a0817
ori agranat <oriagranat9@gmail.com>
2019-03-01 14:42:16 +0200

    websockets?

--- Python/Main.py
+++ Python/Main.py
@@ -1,11 +1,18 @@
 import http.server, socketserver , os
+import websockets, asyncio # pip install websockets
 
 def RunServer():
     WebDir = os.path.join(os.path.dirname(__file__), '../Site')
     os.chdir(WebDir)
     Handler = http.server.SimpleHTTPRequestHandler
     httpd = socketserver.TCPServer(("", 80), Handler)
+    print('server has started')
     httpd.serve_forever()
 
 
+# def WebSocketHandler(socket,path):
+#     data = await websocket.recv()
+#     print (data)
+
+#start_socket = websockets.Serve(WebSocketHandler,'localhost',8765)
 RunServer()
