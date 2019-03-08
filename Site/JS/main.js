f6dc1e8094fa29371deac7a0959d9946c5a2e270
ori agranat <oriagranat9@gmail.com>
2019-03-08 15:55:36 +0200

    fixed an error in websockets and uploded vue
    fixed an error in websockets and uploded vue

--- Site/JS/main.js
+++ Site/JS/main.js
@@ -1,6 +1,6 @@
-let ws = new WebSocket("ws://localhost:8888/websocket");
+var ws = new WebSocket("ws://localhost:8888/websocket");
 // ws.onopen = function () {
-//         ws.send("hello");
+//     ws.send("hello");
 // }
 // ws.onmessage = function (ev) {
 //     alert(ev.data);
