42b37eba418ede4891e21bfd4eff895a6832c23b
ori <oriagranat9@gmail.com>
2019-08-13 13:07:18 -0700

    changed route for easy deploy of website

--- backend/app/ChameleonVisionApp.py
+++ backend/app/ChameleonVisionApp.py
@@ -15,12 +15,12 @@ class ChameleonApplication(tornado.web.Application):
             (r"/", MainHandler),
             (r"/websocket", ChameleonWebSocket),
             (r"/(.*)", tornado.web.StaticFileHandler,
-             {"path": r"{0}".format(os.path.join(os.path.dirname(__file__), "static"))}),
+             {"path": r"{0}".format(os.path.join(os.path.dirname(__file__), "site"))}),
         ]
 
         settings = dict({
-            "template_path": os.path.join(os.path.dirname(__file__), "templates"),
-            "static_path": os.path.join(os.path.dirname(__file__), "static"),
+            "template_path": os.path.join(os.path.dirname(__file__), "site"),
+            "static_path": os.path.join(os.path.dirname(__file__), "site"),
             "debug": True
             }
         )
