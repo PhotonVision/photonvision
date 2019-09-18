965b3953f92eb99ba34f990f25d0c87ea3e95465
Banks Troutman <bankst@roswellrobotics.com>
2019-09-18 03:08:06 -0400

    Separated vision process loop and added PipelineResult class

---
+++ Main/src/main/java/com/chameleonvision/vision/process/PipelineResult.java
@@ -0,0 +1,9 @@
+package com.chameleonvision.vision.process;
+
+public class PipelineResult {
+	public boolean IsValid = false;
+	public double CalibratedX = 0.0;
+	public double CalibratedY = 0.0;
+	public double Pitch = 0.0;
+	public double Yaw = 0.0;
+}
