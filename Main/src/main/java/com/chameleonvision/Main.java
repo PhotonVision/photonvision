7113e5147dfc4238fcc89a35a10fa52974535faa
ori agranat <oriagranat9@gmail.com>
2019-09-25 20:03:41 +0300

    changed version number

--- Main/src/main/java/com/chameleonvision/Main.java
+++ Main/src/main/java/com/chameleonvision/Main.java
@@ -4,6 +4,7 @@ import com.chameleonvision.settings.SettingsManager;
 import com.chameleonvision.vision.camera.CameraManager;
 import com.chameleonvision.vision.process.VisionProcess;
 import com.chameleonvision.web.Server;
+import edu.wpi.first.networktables.NetworkTableInstance;
 
 public class Main {
 	public static void main(String[] args) {
@@ -12,7 +13,7 @@ public class Main {
 			for (var camSet : CameraManager.getAllCamerasByName().entrySet()) {
 				new Thread(new VisionProcess(camSet.getValue())).start();
 			}
-			//  NetworkTableInstance.getDefault().startClientTeam(SettingsManager.GeneralSettings.team_number);
+			  NetworkTableInstance.getDefault().startClientTeam(SettingsManager.GeneralSettings.team_number);
 			Server.main(8888);
 		} else {
 			System.err.println("No cameras connected!");
