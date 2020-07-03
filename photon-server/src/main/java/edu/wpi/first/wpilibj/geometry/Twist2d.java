7a488949b8eb3b191a5a82608952e455d1bb4956
Prateek Machiraju <prateek.machiraju@gmail.com>
2020-07-02 21:58:04 -0400

    Do not apply PhotonVision license to wpilib

--- photon-server/src/main/java/edu/wpi/first/wpilibj/geometry/Twist2d.java
+++ photon-server/src/main/java/edu/wpi/first/wpilibj/geometry/Twist2d.java
@@ -1,19 +1,9 @@
-/*
- * Copyright (C) 2020 Photon Vision.
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
- */
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
 
 package edu.wpi.first.wpilibj.geometry;
 
