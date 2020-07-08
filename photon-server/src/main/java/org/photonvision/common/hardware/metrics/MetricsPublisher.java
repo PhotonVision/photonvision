76afa3f4cb82c64ef2fd42c2433d213ca1218571
Neel <neeljos24@gmail.com>
2020-07-07 21:51:18 -0400

    [Server] Hardware Management

--- photon-server/src/main/java/org/photonvision/common/hardware/metrics/MetricsPublisher.java
+++ photon-server/src/main/java/org/photonvision/common/hardware/metrics/MetricsPublisher.java
@@ -1,8 +1,28 @@
+/*
+ * Copyright (C) 2020 Photon Vision.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package org.photonvision.common.hardware.metrics;
 
 import java.util.HashMap;
 import java.util.Timer;
 import java.util.TimerTask;
+import org.photonvision.common.dataflow.DataChangeService;
+import org.photonvision.common.dataflow.events.OutgoingUIEvent;
+import org.photonvision.server.UIUpdateType;
 
 public class MetricsPublisher {
     private final HashMap<String, Double> metrics;
@@ -32,11 +52,10 @@ public class MetricsPublisher {
                                             metrics.put("gpuTemp", gpu.getTemp());
                                             metrics.put("gpuMem", gpu.getMemory());
                                             metrics.put("ramUtil", ram.getUsedRam());
-                                            /*
+
                                             DataChangeService.getInstance()
                                                     .publishEvent(
                                                             new OutgoingUIEvent<>(UIUpdateType.BROADCAST, "metrics", metrics));
-                                             */
                                         }
                                     },
                                     0,
@@ -51,5 +70,4 @@ public class MetricsPublisher {
     private static class Singleton {
         public static final MetricsPublisher INSTANCE = new MetricsPublisher();
     }
-
 }
