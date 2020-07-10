dc7592fc3f31b43e9e00b511547497672e74eac0
Banks Troutman <btrout.dhrs@gmail.com>
2020-07-10 03:53:48 -0400

    Updated Platform util class, added bootup logging

--- photon-server/src/main/java/org/photonvision/Main.java
+++ photon-server/src/main/java/org/photonvision/Main.java
@@ -26,7 +26,7 @@ import org.photonvision.common.logging.LogLevel;
 import org.photonvision.common.logging.LogGroup;
 import org.photonvision.common.logging.Logger;
 import org.photonvision.common.networking.NetworkManager;
-import org.photonvision.common.util.TestUtils;
+import org.photonvision.common.util.Platform;
 import org.photonvision.server.Server;
 import org.photonvision.vision.camera.USBCameraSource;
 import org.photonvision.vision.pipeline.CVPipelineSettings;
@@ -36,8 +36,11 @@ import org.photonvision.vision.processes.VisionSourceManager;
 
 public class Main {
     private static final Logger logger = new Logger(Main.class, LogGroup.General);
-
     public static final int DEFAULT_WEBPORT = 5800;
+
+    private String getVersion() {
+        return "2020.7.1"; // TODO: grab from gradle/resource somehow
+    }
 
     public static void main(String[] args) {
         Logger.setLevel(LogGroup.Camera, LogLevel.TRACE);
@@ -46,23 +49,41 @@ public class Main {
         Logger.setLevel(LogGroup.Data, LogLevel.TRACE);
         Logger.setLevel(LogGroup.General, LogLevel.TRACE);
 
-        TestUtils.loadLibraries();
+        logger.info("Logging initialized!")
+
+
+        logger.info("Starting PhotonVision version " + getVersion()
+                + " on " + Platform.CurrentPlatform.toString();
+        try {
+            logger.info("Loading native libraries...")
+            CameraServerCvJNI.forceLoad();
+        }
+        catch(Exception e) {
+            logger.error("Failed to load native libraries!");
+            e.printStackTrace(); // TODO: redirect stacktrace to Logger stream somehow
+        }
+        logger.info("Native libaries loaded.")
+
         ConfigManager.getInstance(); // init config manager
         NetworkManager.getInstance().initialize(false); // basically empty. todo: link to ConfigManager?
         NetworkTablesManager.setClientMode("127.0.0.1");
 
         HashMap<String, CameraConfiguration> camConfigs =
                 ConfigManager.getInstance().getConfig().getCameraConfigurations();
-        logger.info("Loaded " + camConfigs.size() + " configs from disk!");
+        logger.info("Loaded " + camConfigs.size() + " configs from disk.");
         List<VisionSource> sources = VisionSourceManager.loadAllSources(camConfigs.values());
 
         var collectedSources = new HashMap<VisionSource, List<CVPipelineSettings>>();
         for (var src : sources) {
             var usbSrc = (USBCameraSource) src;
             collectedSources.put(usbSrc, usbSrc.configuration.pipelineSettings);
+            logger.trace(() -> {
+                return "Matched config for camera \"" + src.getFrameProvider().getName() + "\" and loaded "
+                        + usbSrc.configuration.pipelineSettings.size() + " pipelines");
+            });
         }
 
-        logger.info("Adding " + collectedSources.size() + " configs to VMM");
+        logger.info("Adding " + collectedSources.size() + " configs to VMM.");
         VisionModuleManager.getInstance().addSources(collectedSources);
         ConfigManager.getInstance().addCameraConfigurations(collectedSources);
 
--- photon-server/src/main/java/org/photonvision/common/util/Platform.java
+++ photon-server/src/main/java/org/photonvision/common/util/Platform.java
@@ -22,20 +22,13 @@ import java.io.IOException;
 
 @SuppressWarnings("unused")
 public enum Platform {
-    // WPILib Supported (JNI)
     WINDOWS_32("Windows x32"),
     WINDOWS_64("Windows x64"),
+    LINUX_32("Linux x32"),
     LINUX_64("Linux x64"),
-    LINUX_RASPBIAN("Linux Raspbian"), // Raspberry Pi 3/4
-    LINUX_AARCH64BIONIC("Linux AARCH64 Bionic"), // Jetson Nano, Jetson TX2
-    MACOS_64("Mac OS x64"),
-
-    // ChameleonVision Supported (Manual install)
-    LINUX_ARM32("Linux ARM32"), // ODROID XU4, C1+
-    LINUX_ARM64("Linux ARM64"), // ODROID C2, N2
-
-    // Completely unsupported
-    UNSUPPORTED("Unsupported Platform");
+    LINUX_RASPBIAN("Linux Raspbian"), // TODO: check that RaspiOS reports the same way
+    LINUX_AARCH64BIONIC("Linux Aarch64 Bionic"),
+    UNSUPPORTED(UnsupportedPlatformString);
 
     public final String value;
     public final boolean isRoot = checkForRoot();
@@ -44,23 +37,17 @@ public enum Platform {
         this.value = value;
     }
 
-    private static final String OS_NAME = System.getProperty("os.name");
-    private static final String OS_ARCH = System.getProperty("os.arch");
+    private static String UnsupportedPlatformString =
+            "Unsupported Platform - OS: " + OS_NAME + ", Architecture: " + OS_ARCH;
+
     public static final Platform CurrentPlatform = getCurrentPlatform();
 
-    private static String UnknownPlatformString =
-            String.format("Unknown Platform. OS: %s, Architecture: %s", OS_NAME, OS_ARCH);
-
-    public boolean isWindows() {
-        return this == WINDOWS_64 || this == WINDOWS_32;
+    public static boolean isWindows() {
+        return CurrentPlatform == WINDOWS_64 || CurrentPlatform == WINDOWS_32;
     }
 
-    public boolean isLinux() {
-        return this == LINUX_64 || this == LINUX_RASPBIAN || this == LINUX_ARM64;
-    }
-
-    public boolean isMac() {
-        return this == MACOS_64;
+    public static boolean isLinux() {
+        return CurrentPlatform != UNSUPPORTED && !isWindows();
     }
 
     public static boolean isRaspberryPi() {
@@ -71,7 +58,7 @@ public enum Platform {
 
     @SuppressWarnings("StatementWithEmptyBody")
     private boolean checkForRoot() {
-        if (isLinux() || isMac()) {
+        if (isLinux()) {
             try {
                 shell.execute("id", null, true, "-u");
             } catch (IOException e) {
@@ -98,26 +85,17 @@ public enum Platform {
             if (RuntimeDetector.is64BitIntel()) return WINDOWS_64;
         }
 
-        if (RuntimeDetector.isMac()) {
-            if (RuntimeDetector.is32BitIntel()) return UNSUPPORTED;
-            if (RuntimeDetector.is64BitIntel()) return MACOS_64;
-        }
-
         if (RuntimeDetector.isLinux()) {
-            if (RuntimeDetector.is32BitIntel()) return UNSUPPORTED;
+            if (RuntimeDetector.is32BitIntel()) return LINUX_32;
             if (RuntimeDetector.is64BitIntel()) return LINUX_64;
             if (RuntimeDetector.isRaspbian()) return LINUX_RASPBIAN;
+            if (RuntimeDetector.isAarch64Bionic()) return LINUX_AARCH64BIONIC;
         }
 
-        System.out.println(UnknownPlatformString);
-        return Platform.UNSUPPORTED;
+        return UNSUPPORTED;
     }
 
     public String toString() {
-        if (this.equals(UNSUPPORTED)) {
-            return UnknownPlatformString;
-        } else {
-            return this.value;
-        }
+        return this.value;
     }
 }
