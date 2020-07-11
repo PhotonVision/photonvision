7ad7886ebb01cf1de71d79a92c5752b0b55b7ba9
Banks Troutman <btrout.dhrs@gmail.com>
2020-07-11 19:08:35 -0400

    Add most of the missing NT data to NTDataConsumer

--- photon-server/src/main/java/org/photonvision/common/dataflow/networktables/NTDataConsumer.java
+++ photon-server/src/main/java/org/photonvision/common/dataflow/networktables/NTDataConsumer.java
@@ -26,25 +26,92 @@ import org.photonvision.vision.processes.Data;
 public class NTDataConsumer implements DataConsumer {
 
     private final NetworkTable rootTable;
-    NetworkTable subTable;
-    NetworkTableEntry rawData;
+    private NetworkTable subTable;
+    private NetworkTableEntry rawDataEntry;
+    private NetworkTableEntry pipelineIndexEntry;
+    public NetworkTableEntry driverModeEntry;
+    //private int ntDriveModeListenerID; // TODO: NTDataPublisher
+    //private int ntPipelineListenerID; // TODO: NTDataPublisher
+    private NetworkTableEntry latencyMillisEntry;
+    private NetworkTableEntry hasTargetEntry;
+    private NetworkTableEntry targetPitchEntry;
+    private NetworkTableEntry targetYawEntry;
+    private NetworkTableEntry targetAreaEntry;
+    private NetworkTableEntry targetPoseEntry;
+
+    // TODO: what are these, and do we need them?
+    private NetworkTableEntry targetFittedHeightEntry;
+    private NetworkTableEntry targetFittedWidthEntry;
+    private NetworkTableEntry targetBoundingHeightEntry;
+    private NetworkTableEntry targetBoundingWidthEntry;
+    private NetworkTableEntry targetRotationEntry;
 
     public NTDataConsumer(NetworkTable root, String camName) {
-        this.rootTable = root;
-        this.subTable = root.getSubTable(camName);
-        rawData = subTable.getEntry("rawData");
+        rootTable = root;
+        subTable = root.getSubTable(camName);
+        rawDataEntry = subTable.getEntry("rawData");
+
+        pipelineIndexEntry = subTable.getEntry("pipelineIndex");
+        driverModeEntry = subTable.getEntry("driverMode");
+        latencyMillisEntry = subTable.getEntry("latencyMillis");
+        hasTargetEntry = subTable.getEntry("hasTarget");
+
+        targetPitchEntry = subTable.getEntry("targetPitch");
+        targetAreaEntry = subTable.getEntry("targetArea");
+        targetYawEntry = subTable.getEntry("targetYaw");
+        targetPoseEntry = subTable.getEntry("targetPose");
+//        targetFittedHeightEntry = subTable.getEntry("targetFittedHeight");
+//        targetFittedWidthEntry = subTable.getEntry("targetFittedWidth");
+//        targetBoundingHeightEntry = subTable.getEntry("targetBoundingHeight");
+//        targetBoundingWidthEntry = subTable.getEntry("targetBoundingWidth");
+//        targetRotationEntry = subTable.getEntry("targetRotation");
     }
 
     public void setCameraName(String camName) {
-        this.subTable = rootTable.getSubTable(camName);
-        rawData = subTable.getEntry("rawData");
+        subTable = rootTable.getSubTable(camName);
+        rawDataEntry = subTable.getEntry("rawData");
+
+        pipelineIndexEntry = subTable.getEntry("pipelineIndex");
+        driverModeEntry = subTable.getEntry("driverMode");
+        latencyMillisEntry = subTable.getEntry("latencyMillis");
+        hasTargetEntry = subTable.getEntry("hasTarget");
+
+        targetPitchEntry = subTable.getEntry("targetPitch");
+        targetAreaEntry = subTable.getEntry("targetArea");
+        targetYawEntry = subTable.getEntry("targetYaw");
+        targetPoseEntry = subTable.getEntry("targetPose");
+//        targetFittedHeightEntry = subTable.getEntry("targetFittedHeight");
+//        targetFittedWidthEntry = subTable.getEntry("targetFittedWidth");
+//        targetBoundingHeightEntry = subTable.getEntry("targetBoundingHeight");
+//        targetBoundingWidthEntry = subTable.getEntry("targetBoundingWidth");
+        //targetRotationEntry = subTable.getEntry("targetRotation")
     }
 
     @Override
     public void accept(Data data) {
-        var simplified = new SimplePipelineResult(data.result);
+        var result = data.result;
+        // send raw data immediately
+        var simplified = new SimplePipelineResult(result);
         var bytes = simplified.toByteArray();
-        rawData.setRaw(bytes);
+        rawDataEntry.forceSetRaw(bytes);
         rootTable.getInstance().flush();
+
+        // send keyed data after raw
+        // TODO: get pipelineIndex, driverMode
+
+        latencyMillisEntry.forceSetDouble(result.getLatencyMillis());
+        hasTargetEntry.forceSetBoolean(result.hasTargets());
+
+        // TODO: send all zeroes if no targets?
+        var bestTarget = result.targets.get(0);
+
+        targetPitchEntry.forceSetDouble(bestTarget.getPitch());
+        targetYawEntry.forceSetDouble(bestTarget.getYaw());
+        targetAreaEntry.forceSetDouble(bestTarget.getArea());
+
+        var poseX = bestTarget.getRobotRelativePose().getTranslation().getX();
+        var poseY = bestTarget.getRobotRelativePose().getTranslation().getY();
+        var poseRot = bestTarget.getRobotRelativePose().getRotation().getDegrees();
+        targetPoseEntry.forceSetDoubleArray(new double[] { poseX, poseY, poseRot });
     }
 }
