221ec6d4e68ac6514ef0d62b902761072201abad
Matt <matthew.morley.ca@gmail.com>
2020-07-17 14:03:07 -0700

    Make input and output both rotated

--- photon-server/src/main/java/org/photonvision/vision/pipeline/ReflectivePipeline.java
+++ photon-server/src/main/java/org/photonvision/vision/pipeline/ReflectivePipeline.java
@@ -165,11 +165,12 @@ public class ReflectivePipeline extends CVPipeline<CVPipelineResult, ReflectiveP
 
         long sumPipeNanosElapsed = 0L;
 
-        rawInputMat.release();
-        frame.image.getMat().copyTo(rawInputMat);
 
         CVPipeResult<Mat> rotateImageResult = rotateImagePipe.apply(frame.image.getMat());
         sumPipeNanosElapsed += rotateImageResult.nanosElapsed;
+
+        rawInputMat.release();
+        frame.image.getMat().copyTo(rawInputMat);
 
         CVPipeResult<Mat> erodeDilateResult = erodeDilatePipe.apply(rotateImageResult.result);
         sumPipeNanosElapsed += erodeDilateResult.nanosElapsed;
