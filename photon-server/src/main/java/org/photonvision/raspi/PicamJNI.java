b4364fb0eaa86c3a4122b6d89b27f830e313571e
Matt <matthew.morley.ca@gmail.com>
2020-07-20 16:45:55 -0700

    use runtimeloader

--- photon-server/src/main/java/org/photonvision/raspi/PicamJNI.java
+++ photon-server/src/main/java/org/photonvision/raspi/PicamJNI.java
@@ -1,8 +1,27 @@
 package org.photonvision.raspi;
 
+import edu.wpi.first.wpiutil.RuntimeLoader;
+import edu.wpi.first.wpiutil.WPIUtilJNI;
+
+import java.io.IOException;
+
 public class PicamJNI {
+
+    static RuntimeLoader<PicamJNI> loader;
+    private static boolean libraryLoaded;
+
     static {
-        System.loadLibrary("libpicam"); // Load native library libpicam.so
+//        System.loadLibrary("libpicam"); // Load native library libpicam.so
+
+        try {
+            loader = new RuntimeLoader<>("wpiutiljni", RuntimeLoader.getDefaultExtractionRoot(), PicamJNI.class);
+            loader.loadLibrary();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            System.exit(1);
+        }
+        libraryLoaded = true;
+
     }
 
     // Declare an instance native method sayHello() which receives no parameter and returns void
