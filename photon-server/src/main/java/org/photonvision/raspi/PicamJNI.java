eaae5dbf3eea88412be71d040ca7d5eb9d48690f
= <matthew.morley.ca@gmail.com>
2020-07-21 06:28:39 +0100

    Switch JNI methods to static

--- photon-server/src/main/java/org/photonvision/raspi/PicamJNI.java
+++ photon-server/src/main/java/org/photonvision/raspi/PicamJNI.java
@@ -2,28 +2,35 @@ package org.photonvision.raspi;
 
 import java.io.IOException;
 import java.nio.file.Path;
+import org.opencv.core.Mat;
+import org.opencv.core.CvType;
+import org.photonvision.common.util.TestUtils;
 
 public class PicamJNI {
 
     static {
        System.load(Path.of("src/main/resources/native/libpicam.so").toAbsolutePath().toString());
+       TestUtils.loadLibraries();
     }
 
     public static void main(String[] args) {
-        var cam = new PicamJNI();
-        cam.setVideoMode(1920, 1080, 30); 
-        cam.setExposure(10);
-        cam.createCamera();
+       
+        // PicamJNI.setVideoMode(1920, 1080, 30); 
+        // PicamJNI.setExposure(10);
+        var mat = new Mat(1080, 1920, CvType.CV_8UC1);
+        PicamJNI.grabFrame(mat.nativeObj);
+
+        // cam.createCamera();
     }
 
-    private native boolean createCamera();
-    private native boolean destroyCamera();
+    private static native boolean createCamera();
+    private static native boolean destroyCamera();
 
-    private native boolean setExposure(int exposure);
-    private native boolean setBrightness(int exposure);
-    private native boolean setISO(int iso);
-    private native boolean setRotation(int rotation);
-    private native boolean setVideoMode(int width, int height, int fps);
+    private static native boolean setExposure(int exposure);
+    private static native boolean setBrightness(int exposure);
+    private static native boolean setISO(int iso);
+    private static native boolean setRotation(int rotation);
+    private static native boolean setVideoMode(int width, int height, int fps);
 
-    private native boolean grabFrame(long imageNativeObj);
+    private static native boolean grabFrame(long imageNativeObj);
 }
