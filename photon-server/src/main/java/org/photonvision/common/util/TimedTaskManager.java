447695838f4a626fb401eadca1f9d754769bed6e
Banks Troutman <btrout.dhrs@gmail.com>
2020-07-23 21:55:06 -0400

    Apply Spotless

--- photon-server/src/main/java/org/photonvision/common/util/TimedTaskManager.java
+++ photon-server/src/main/java/org/photonvision/common/util/TimedTaskManager.java
@@ -1,12 +1,27 @@
-package org.photonvision.common.util;
+/*
+ * Copyright (C) 2020 Photon Vision.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 
-import org.jetbrains.annotations.NotNull;
-import org.photonvision.common.logging.LogGroup;
-import org.photonvision.common.logging.Logger;
+package org.photonvision.common.util;
 
 import java.util.Arrays;
 import java.util.concurrent.*;
-import java.util.stream.Stream;
+import org.jetbrains.annotations.NotNull;
+import org.photonvision.common.logging.LogGroup;
+import org.photonvision.common.logging.Logger;
 
 public class TimedTaskManager {
 
@@ -33,34 +48,37 @@ public class TimedTaskManager {
     }
 
     private class CaughtThreadFactory implements ThreadFactory {
-       @Override
-       public Thread newThread(@NotNull Runnable r) {
-           String taskIdentifier = "Unknown";
-           for (TimedTask timedTask : activeTasks.values()) {
-               if (timedTask.runnable == r) {
-                   taskIdentifier = timedTask.identifier;
-                   break;
-               }
-           }
+        @Override
+        public Thread newThread(@NotNull Runnable r) {
+            String taskIdentifier = "Unknown";
+            for (TimedTask timedTask : activeTasks.values()) {
+                if (timedTask.runnable == r) {
+                    taskIdentifier = timedTask.identifier;
+                    break;
+                }
+            }
 
-           var errorString = "Exception running task \"" + taskIdentifier + "\": ";
-           return new Thread(() -> {
-               try {
-                   r.run();
-               } catch (Throwable t) {
-                   logger.error(errorString);
-                   logger.error(Arrays.toString(t.getStackTrace()));
-               }
-           });
-       }
+            var errorString = "Exception running task \"" + taskIdentifier + "\": ";
+            return new Thread(
+                    () -> {
+                        try {
+                            r.run();
+                        } catch (Throwable t) {
+                            logger.error(errorString);
+                            logger.error(Arrays.toString(t.getStackTrace()));
+                        }
+                    });
+        }
     }
 
-    private final ScheduledExecutorService taskExecutor = Executors.newScheduledThreadPool(2, new CaughtThreadFactory());
+    private final ScheduledExecutorService taskExecutor =
+            Executors.newScheduledThreadPool(2, new CaughtThreadFactory());
     private final ConcurrentHashMap<String, TimedTask> activeTasks = new ConcurrentHashMap<>();
 
     public void addTask(String identifier, Runnable runnable, long millisInterval) {
         if (!activeTasks.containsKey(identifier)) {
-            var future = taskExecutor.scheduleAtFixedRate(runnable, 0, millisInterval, TimeUnit.MILLISECONDS);
+            var future =
+                    taskExecutor.scheduleAtFixedRate(runnable, 0, millisInterval, TimeUnit.MILLISECONDS);
             activeTasks.put(identifier, new TimedTask(identifier, runnable, future));
         }
     }
