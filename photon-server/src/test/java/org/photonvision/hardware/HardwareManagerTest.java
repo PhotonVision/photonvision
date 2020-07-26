8d71b74093926e190658e973b2c766d003c7872e
Neel <neeljos24@gmail.com>
2020-07-26 00:08:17 -0400

    [Hardware] Fix PWM port

--- photon-server/src/test/java/org/photonvision/hardware/HardwareManagerTest.java
+++ photon-server/src/test/java/org/photonvision/hardware/HardwareManagerTest.java
@@ -37,9 +37,9 @@ public class HardwareManagerTest {
 
         var instance = HardwareManager.getInstance();
 
-        instance.getPWM(2).setPwmRange(List.of(5, 99));
-        Assertions.assertEquals(instance.getPWM(2).getPwmRange().get(0), 5);
-        Assertions.assertEquals(instance.getPWM(2).getPwmRange().get(1), 99);
+        instance.getPWM(13).setPwmRange(List.of(5, 99));
+        Assertions.assertEquals(instance.getPWM(13).getPwmRange().get(0), 5);
+        Assertions.assertEquals(instance.getPWM(13).getPwmRange().get(1), 99);
         instance.getGPIO(2).blink(250, 2000);
         for (int i = 0; i < 101; i++) {
             instance.getPWM(13).dimLED(i);
