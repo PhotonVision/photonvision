40c14d027d41143eb17f787d4b3c0346baa829c3
Matt <matthew.morley.ca@gmail.com>
2020-07-30 11:50:23 -0600

    Remove non-java classes from the picam jni

--- photon-server/src/main/java/org/photonvision/raspi/PicamJNI.java
+++ photon-server/src/main/java/org/photonvision/raspi/PicamJNI.java
@@ -1,26 +1,11 @@
 package org.photonvision.raspi;
 
-import java.io.IOException;
 import java.nio.file.Path;
-import org.opencv.core.Mat;
-import org.opencv.core.CvType;
-import org.photonvision.common.util.TestUtils;
 
 public class PicamJNI {
 
     static {
        System.load(Path.of("src/main/resources/native/libpicam.so").toAbsolutePath().toString());
-       TestUtils.loadLibraries();
-    }
-
-    public static void main(String[] args) {
-       
-        // PicamJNI.setVideoMode(1920, 1080, 30); 
-        // PicamJNI.setExposure(10);
-        var mat = new Mat(1080, 1920, CvType.CV_8UC1);
-        PicamJNI.grabFrame(mat.nativeObj);
-
-        // cam.createCamera();
     }
 
     private static native boolean createCamera();
