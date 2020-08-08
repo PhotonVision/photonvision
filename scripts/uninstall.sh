61ab1b2bd2b9b72239341ed7823f54bdb7627c94
Prateek Machiraju <prateek.machiraju@gmail.com>
2020-08-07 22:40:22 -0400

    Add uninstall script (#78)

---
+++ scripts/uninstall.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [ "$(id -u)" != "0" ]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+
+rm -rf /opt/photonvision/
+echo "Uninstalling systemd service..."
+systemctl stop photonvision
+systemctl disable photonvision
+rm /lib/systemd/system/photonvision.service
+rm /etc/systemd/system/photonvision.service
+systemctl dameon-relaod 
+systemctl reset-failed
