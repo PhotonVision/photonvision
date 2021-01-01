28e3b510c70bdbb3d4172d7a3262f361e7758c0b
Matt <matthew.morley.ca@gmail.com>
2020-12-31 19:57:51 -0800

    Remove year from header (#207)
    Yeet the year

--- photon-server/src/test/java/org/photonvision/vision/opencv/ContourTest.java
+++ photon-server/src/test/java/org/photonvision/vision/opencv/ContourTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2020 Photon Vision.
+ * Copyright (C) Photon Vision.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
