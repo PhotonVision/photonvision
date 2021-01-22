52a97b0a14ffe904cf3bb932dee595a01c8cfb6e
Chris <chrisgerth010592@gmail.com>
2021-01-22 14:08:13 -0600

    Added simulation-supporting aim and range example

---
+++ photonlib-cpp-examples/src/main/cpp/examples/simaimandrange/cpp/Robot.cpp
@@ -0,0 +1,72 @@
+/**
+ * Copyright (C) 2018-2020 Photon Vision.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include "Robot.h"
+
+#include <photonlib/PhotonUtils.h>
+
+void Robot::TeleopPeriodic() {
+  double forwardSpeed;
+  double rotationSpeed;
+
+  if (xboxController.GetAButton()) {
+    // Vision-alignment mode
+    // Query the latest result from PhotonVision
+    const auto &result = camera.GetLatestResult();
+
+    if (result.HasTargets()) {
+      // First calculate range
+      units::meter_t range =
+          photonlib::PhotonUtils::CalculateDistanceToTarget(
+              CAMERA_HEIGHT, TARGET_HEIGHT, CAMERA_PITCH,
+              units::degree_t{result.GetBestTarget().GetPitch()});
+
+      // Use this range as the measurement we give to the PID controller.
+      // -1.0 required to ensure positive PID controller effort _increases_ range
+      forwardSpeed = -1.0* forwardController.Calculate(
+          range.to<double>(), GOAL_RANGE_METERS.to<double>());
+
+      // Also calculate angular power
+      // -1.0 required to ensure positive PID controller effort _increases_ yaw
+      rotationSpeed = -1.0 * turnController.Calculate(
+          result.GetBestTarget().GetYaw(), 0);
+    } else {
+      // If we have no targets, stay still.
+      forwardSpeed = 0;
+      rotationSpeed = 0;
+    }
+  } else {
+    // Manual Driver Mode
+    forwardSpeed =
+        -1.0 * xboxController.GetY(frc::GenericHID::JoystickHand::kLeftHand);
+    rotationSpeed =
+        xboxController.GetX(frc::GenericHID::JoystickHand::kRightHand);
+  }
+
+  // Use our forward/turn speeds to control the drivetrain
+  drive.ArcadeDrive(forwardSpeed, rotationSpeed);
+}
+
+void Robot::SimulationPeriodic() {
+    dtSim.update();
+}
+
+#ifndef RUNNING_FRC_TESTS
+int main() { return frc::StartRobot<Robot>(); }
+#endif
+
+
---
+++ photonlib-cpp-examples/src/main/cpp/examples/simaimandrange/cpp/sim/DrivetrainSim.cpp
@@ -0,0 +1,43 @@
+/**
+ * Copyright (C) 2018-2020 Photon Vision.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include "DrivetrainSim.h"
+
+/**
+* Perform all periodic drivetrain simulation related tasks to advance our simulation of robot
+* physics forward by a single 20ms step.
+*/
+void DrivetrainSim::update(){
+
+    double leftMotorCmd = 0;
+    double rightMotorCmd = 0;
+
+    if (frc::DriverStation::GetInstance().IsEnabled() && !frc::RobotController::IsBrownedOut()) {
+        leftMotorCmd = leftLeader.GetSpeed();
+        rightMotorCmd = rightLeader.GetSpeed();
+    } 
+    
+    m_drivetrainSimulator.SetInputs(
+            units::volt_t(leftMotorCmd * frc::RobotController::GetInputVoltage()),
+            units::volt_t(-rightMotorCmd * frc::RobotController::GetInputVoltage()) );
+    m_drivetrainSimulator.Update(20_ms);
+
+    // Update PhotonVision based on our new robot position.
+    simVision.ProcessFrame(m_drivetrainSimulator.GetPose());
+
+    field.SetRobotPose(m_drivetrainSimulator.GetPose());
+}
