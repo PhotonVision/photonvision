9dc14c5ac8b0b063e034ac20c778753488d8756e
Banks Troutman <btrout.dhrs@gmail.com>
2021-03-22 18:23:33 -0400

    formatting hell 2 electric boogaloo

--- photon-lib/src/main/java/org/photonvision/SimPhotonCamera.java
+++ photon-lib/src/main/java/org/photonvision/SimPhotonCamera.java
@@ -20,10 +20,8 @@ package org.photonvision;
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
-
 import java.util.Arrays;
 import java.util.List;
-
 import org.photonvision.common.dataflow.structures.Packet;
 import org.photonvision.targeting.PhotonPipelineResult;
 import org.photonvision.targeting.PhotonTrackedTarget;
@@ -39,10 +37,10 @@ public class SimPhotonCamera extends PhotonCamera {
     private final NetworkTableEntry targetPoseEntry;
 
     /**
-     * Constructs a Simulated PhotonCamera from a root table.
-     *
-     * @param rootTable The root table that the camera is broadcasting information over.
-     */
+    * Constructs a Simulated PhotonCamera from a root table.
+    *
+    * @param rootTable The root table that the camera is broadcasting information over.
+    */
     public SimPhotonCamera(NetworkTable rootTable) {
         super(rootTable);
 
@@ -56,53 +54,53 @@ public class SimPhotonCamera extends PhotonCamera {
     }
 
     /**
-     * Constructs a Simulated PhotonCamera from the name of the camera.
-     *
-     * @param cameraName The nickname of the camera (found in the PhotonVision UI).
-     */
+    * Constructs a Simulated PhotonCamera from the name of the camera.
+    *
+    * @param cameraName The nickname of the camera (found in the PhotonVision UI).
+    */
     public SimPhotonCamera(String cameraName) {
         this(NetworkTableInstance.getDefault().getTable("photonvision").getSubTable(cameraName));
     }
 
     /**
-     * Simulate one processed frame of vision data, putting one result to NT.
-     *
-     * @param latencyMillis Latency of the provided frame
-     * @param targets       Each target detected
-     */
+    * Simulate one processed frame of vision data, putting one result to NT.
+    *
+    * @param latencyMillis Latency of the provided frame
+    * @param targets Each target detected
+    */
     public void submitProcessedFrame(double latencyMillis, PhotonTrackedTarget... targets) {
         submitProcessedFrame(latencyMillis, Arrays.asList(targets));
     }
 
     /**
-     * Simulate one processed frame of vision data, putting one result to NT.
-     *
-     * @param latencyMillis Latency of the provided frame
-     * @param sortMode      Order in which to sort targets
-     * @param targets       Each target detected
-     */
+    * Simulate one processed frame of vision data, putting one result to NT.
+    *
+    * @param latencyMillis Latency of the provided frame
+    * @param sortMode Order in which to sort targets
+    * @param targets Each target detected
+    */
     public void submitProcessedFrame(
             double latencyMillis, PhotonTargetSortMode sortMode, PhotonTrackedTarget... targets) {
         submitProcessedFrame(latencyMillis, sortMode, Arrays.asList(targets));
     }
 
     /**
-     * Simulate one processed frame of vision data, putting one result to NT.
-     *
-     * @param latencyMillis Latency of the provided frame
-     * @param targetList    List of targets detected
-     */
+    * Simulate one processed frame of vision data, putting one result to NT.
+    *
+    * @param latencyMillis Latency of the provided frame
+    * @param targetList List of targets detected
+    */
     public void submitProcessedFrame(double latencyMillis, List<PhotonTrackedTarget> targetList) {
         submitProcessedFrame(latencyMillis, null, targetList);
     }
 
     /**
-     * Simulate one processed frame of vision data, putting one result to NT.
-     *
-     * @param latencyMillis Latency of the provided frame
-     * @param sortMode      Order in which to sort targets
-     * @param targetList    List of targets detected
-     */
+    * Simulate one processed frame of vision data, putting one result to NT.
+    *
+    * @param latencyMillis Latency of the provided frame
+    * @param sortMode Order in which to sort targets
+    * @param targetList List of targets detected
+    */
     public void submitProcessedFrame(
             double latencyMillis, PhotonTargetSortMode sortMode, List<PhotonTrackedTarget> targetList) {
         latencyMillisEntry.setDouble(latencyMillis);
@@ -122,7 +120,7 @@ public class SimPhotonCamera extends PhotonCamera {
             targetPitchEntry.setDouble(0.0);
             targetYawEntry.setDouble(0.0);
             targetAreaEntry.setDouble(0.0);
-            targetPoseEntry.setDoubleArray(new double[]{0.0, 0.0, 0.0});
+            targetPoseEntry.setDoubleArray(new double[] {0.0, 0.0, 0.0});
             targetSkewEntry.setDouble(0.0);
         } else {
             var bestTarget = newResult.getBestTarget();
@@ -134,7 +132,7 @@ public class SimPhotonCamera extends PhotonCamera {
 
             var transform = bestTarget.getCameraToTarget();
             double[] poseData = {
-                    transform.getX(), transform.getY(), transform.getRotation().getDegrees()
+                transform.getX(), transform.getY(), transform.getRotation().getDegrees()
             };
             targetPoseEntry.setDoubleArray(poseData);
         }
