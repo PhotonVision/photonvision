081880a574c2d442a34c64f8e539a68a8a8e95f7
Tyler Veness <calcmogul@gmail.com>
2021-09-01 22:52:53 -0700

    Fix uninitialized variables in PhotonPipelineResult
    If the default constructor is used, some member variables weren't
    properly initialized.

--- photon-lib/src/main/java/org/photonvision/SimVisionTarget.java
+++ photon-lib/src/main/java/org/photonvision/SimVisionTarget.java
@@ -24,7 +24,6 @@ public class SimVisionTarget {
     double targetWidthMeters;
     double targetHeightMeters;
     double targetHeightAboveGroundMeters;
-    double targetInfill_pct;
     double tgtAreaMeters2;
 
     /**
