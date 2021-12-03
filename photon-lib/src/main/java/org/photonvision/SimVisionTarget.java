0791b38e2bdb0043efff3c4bc469a0309554d54d
Jeffrey Morris <ffejcsirrom@gmail.com>
2021-12-02 19:44:47 -0600

    Add predefined target constructor #269

--- photon-lib/src/main/java/org/photonvision/SimVisionTarget.java
+++ photon-lib/src/main/java/org/photonvision/SimVisionTarget.java
@@ -17,6 +17,7 @@
 package org.photonvision;
 
 import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
 
 public class SimVisionTarget {
     Pose2d targetPos;
@@ -24,6 +25,17 @@ public class SimVisionTarget {
     double targetHeightMeters;
     double targetHeightAboveGroundMeters;
     double tgtAreaMeters2;
+
+    /**
+    * Different pre-defined vision targets, used in constructor to create a target in its official
+    * position from center of feild.
+    */
+    public enum VisionTarget {
+        InfinateRechargeRedAlliancePowerPort,
+        InfinateRechargeRedAllianceLoadingBay,
+        InfinateRechargeBlueAlliancePowerPort,
+        InfinateRechargeBlueAllianceLoadingBay
+    }
 
     /**
     * Describes a vision target located somewhere on the field that your SimVisionSystem can detect.
@@ -45,5 +57,48 @@ public class SimVisionTarget {
         this.targetWidthMeters = targetWidthMeters;
         this.targetHeightMeters = targetHeightMeters;
         this.tgtAreaMeters2 = targetWidthMeters * targetHeightMeters;
+    }
+
+    /**
+    * Create a pre-defined target
+    *
+    * <p>Center of the target is placed as if you were standing at the center of the field facing the
+    * blue alliance wall. positive x in front of you, positive y to the left.
+    *
+    * @param visionTarget the type of target to create
+    */
+    public SimVisionTarget(VisionTarget visionTarget) {
+        switch (visionTarget) {
+            case InfinateRechargeRedAlliancePowerPort:
+                this.targetPos = new Pose2d(7.9916, -1.7052, new Rotation2d()); // x: 314.63" y: 67.134"
+                this.targetHeightAboveGroundMeters = 2.06375; // 6'9.25"
+                this.targetWidthMeters = 0.99695; // 3'3.25"
+                this.targetHeightMeters = 0.4318; // 1'5"
+                this.tgtAreaMeters2 = this.targetWidthMeters * this.targetHeightMeters;
+                break;
+            case InfinateRechargeRedAllianceLoadingBay:
+                this.targetPos = new Pose2d(-7.9916, -1.5494, new Rotation2d()); // x: 314.63" y: 61"
+                this.targetHeightAboveGroundMeters = 0.2794; // 11"
+                this.targetWidthMeters = 0.1778; // 7"
+                this.targetHeightMeters = 0.2794; // 11"
+                this.tgtAreaMeters2 = this.targetWidthMeters * this.targetHeightMeters;
+                break;
+            case InfinateRechargeBlueAlliancePowerPort:
+                this.targetPos = new Pose2d(-7.9916, 1.7052, new Rotation2d()); // x: 314.63" y: 67.134"
+                this.targetHeightAboveGroundMeters = 2.06375; // 6'9.25"
+                this.targetWidthMeters = 0.99695; // 3'3.25"
+                this.targetHeightMeters = 0.4318; // 1'5"
+                this.tgtAreaMeters2 = this.targetWidthMeters * this.targetHeightMeters;
+                break;
+            case InfinateRechargeBlueAllianceLoadingBay:
+                this.targetPos = new Pose2d(7.9916, 1.5494, new Rotation2d()); // x: 314.63" y: 61"
+                this.targetHeightAboveGroundMeters = 0.2794; // 11"
+                this.targetWidthMeters = 0.1778; // 7"
+                this.targetHeightMeters = 0.2794; // 11"
+                this.tgtAreaMeters2 = this.targetWidthMeters * this.targetHeightMeters;
+                break;
+            default:
+                throw new IllegalArgumentException("Invalid VisionTarget");
+        }
     }
 }
