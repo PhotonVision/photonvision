9893cf1f7ed1ce7f02198474237387ecbbee2584
Tyler Veness <calcmogul@gmail.com>
2022-01-20 19:35:28 -0800

    Update photonlib and photonlib example license headers to MIT (#395)

--- photon-core/src/main/java/org/photonvision/vision/pipe/impl/FindBoardCornersPipe.java
+++ photon-core/src/main/java/org/photonvision/vision/pipe/impl/FindBoardCornersPipe.java
@@ -14,6 +14,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
+
 package org.photonvision.vision.pipe.impl;
 
 import java.util.Objects;
