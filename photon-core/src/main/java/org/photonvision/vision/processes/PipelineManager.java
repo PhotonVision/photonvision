30a21a6b4bfefe9f52e3dfd46ff640eab15327dc
shueja-personal <32416547+shueja-personal@users.noreply.github.com>
2022-04-23 20:01:58 -0700

    Fix #461 (pipeline type change index)

--- photon-core/src/main/java/org/photonvision/vision/processes/PipelineManager.java
+++ photon-core/src/main/java/org/photonvision/vision/processes/PipelineManager.java
@@ -400,6 +400,7 @@ public class PipelineManager {
         }
 
         logger.info("Adding new pipe of type " + type.toString() + " at idx " + idx);
+        newSettings.pipelineIndex = idx;
         userPipelineSettings.set(idx, newSettings);
         setPipelineInternal(idx);
     }
