e1f1626a5984530a04bcbe2de42463f46bb9caf4
Andrew Gasser <agasser@users.noreply.github.com>
2022-10-16 20:13:33 -0500

    Change double literal to not use suffix

--- photonlib-java-examples/src/main/java/org/photonlib/examples/simposeest/robot/DrivetrainPoseEstimator.java
+++ photonlib-java-examples/src/main/java/org/photonlib/examples/simposeest/robot/DrivetrainPoseEstimator.java
@@ -86,7 +86,7 @@ public class DrivetrainPoseEstimator {
 
         var res = cam.getLatestResult();
         if (res.hasTargets()) {
-            double imageCaptureTime = Timer.getFPGATimestamp() - res.getLatencyMillis() / 1000d;
+            double imageCaptureTime = Timer.getFPGATimestamp() - res.getLatencyMillis() / 1000.0;
             Transform3d camToTargetTrans = res.getBestTarget().getCameraToTarget();
             var transform =
                     new Transform2d(
