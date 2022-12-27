548f52e11702326e74474a8dd8ef25f814e81fd9
Mohammad Durrani <46766905+mdurrani808@users.noreply.github.com>
2022-12-26 21:40:36 -0500

    Allow JAR update/restart on all linux platforms (#651)
    * Added linux support
    
    * Changed to just check linux

--- photon-core/src/main/java/org/photonvision/common/hardware/HardwareManager.java
+++ photon-core/src/main/java/org/photonvision/common/hardware/HardwareManager.java
@@ -126,7 +126,7 @@ public class HardwareManager {
     }
 
     public boolean restartDevice() {
-        if (Platform.isRaspberryPi()) {
+        if (Platform.isLinux()) {
             try {
                 return shellExec.executeBashCommand("reboot now") == 0;
             } catch (IOException e) {
