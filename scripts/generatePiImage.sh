5c80e64f4bdc4d03be891832d06ac30a78f43813
Matt <matthew.morley.ca@gmail.com>
2022-12-27 07:44:31 -0800

    Potentially fix releaser artifact path
    Don't publish photonlib
    
    Update main.yml
    
    Update main.yml

--- scripts/generatePiImage.sh
+++ scripts/generatePiImage.sh
@@ -2,7 +2,7 @@
 # Not that it really matters, but it'll save us 50 megs or so
 NEW_JAR=$(realpath $(find . -name photonvision\*-linuxarm32.jar))
 echo "Using jar: " $NEW_JAR
-sudo apt install xz-utils
+sudo apt-get install -y xz-utils
 curl -sk https://api.github.com/repos/photonvision/photon-pi-gen/releases/tags/v2023.1.0-beta-1 | grep "browser_download_url.*xz" | cut -d : -f 2,3 | tr -d '"' | wget -qi -
 ls
 FILE_NAME=$(ls | grep image_*.xz)
