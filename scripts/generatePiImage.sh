6b8229d3d8b4a6ff1b905b0dc7ce842b938417a2
Matt <matthew.morley.ca@gmail.com>
2022-12-30 16:31:32 -0800

    Add archive suffix to generator

--- scripts/generatePiImage.sh
+++ scripts/generatePiImage.sh
@@ -1,5 +1,5 @@
-if [ "$#" -ne 1 ]; then
-    echo "Illegal number of parameters"
+if [ "$#" -ne 2 ]; then
+    echo "Illegal number of parameters -- expected (Image release URL) (image suffix)"
     exit 1
 fi
 
@@ -10,20 +10,23 @@ NEW_JAR=$(realpath $(find . -name photonvision\*-linuxarm64.jar))
 echo "Using jar: " $NEW_JAR
 echo "Downloading image from " $1
 sudo apt-get install -y xz-utils
-curl -sk $1 | grep "browser_download_url.*xz" | cut -d : -f 2,3 | tr -d '"' | wget -i -
+# curl -sk $1 | grep "browser_download_url.*xz" | cut -d : -f 2,3 | tr -d '"' | wget -qi -
 ls
 FILE_NAME=$(ls | grep image_*.xz)
-echo "Downloaded " $FILE_NAME
+echo "Downloaded " $FILE_NAME " -- decompressing now..."
 xz -T0 -v --decompress $FILE_NAME
 IMAGE_FILE=$(ls | grep *.img)
 ls
-echo "Unziped image: " $IMAGE_FILE
+echo "Unziped image: " $IMAGE_FILE " -- mounting"
 TMP=$(mktemp -d)
 LOOP=$(sudo losetup --show -fP "${IMAGE_FILE}")
+echo "Image mounted! Copying jar..."
 sudo mount ${LOOP}p2 $TMP
 pushd .
 cd $TMP/opt/photonvision
 sudo cp $NEW_JAR photonvision.jar
+
+echo "Jar updated! Creating service..."
 
 cd $TMP/etc/systemd/system/multi-user.target.wants
 sudo bash -c "printf \
@@ -42,9 +45,13 @@ RestartSec=1
 WantedBy=multi-user.target\" > photonvision.service"
 
 popd
+
+echo "Service created!"
+
 sudo umount ${TMP}
 sudo rmdir ${TMP}
-NEW_IMAGE=$(basename "${NEW_JAR/jar/img}")
+NEW_IMAGE=$(basename "${NEW_JAR/.jar/_$2.img}")
+echo "Renaming image " $IMAGE_FILE " -> " $NEW_IMAGE
 mv $IMAGE_FILE $NEW_IMAGE
 xz -T0 -v -z $NEW_IMAGE
-mv $NEW_IMAGE.xz $(basename "${NEW_JAR/.jar/-image.xz}")
+mv $NEW_IMAGE.xz $(basename "${NEW_JAR/.jar/-image_$2.xz}")
