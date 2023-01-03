f362f97c7c4edfa274eb4156e102a3e0cbc57df6
Sriman Achanta <68172138+srimanachanta@users.noreply.github.com>
2023-01-03 13:51:52 -0500

    Update RobotPoseEstimator.h

--- photon-lib/src/main/native/include/photonlib/RobotPoseEstimator.h
+++ photon-lib/src/main/native/include/photonlib/RobotPoseEstimator.h
@@ -83,6 +83,17 @@ class RobotPoseEstimator {
   }
 
   /**
+   * Set the cameras to be used by the PoseEstimator.
+   * 
+   * @param cameras cameras to set.
+  */
+  inline void SetCameras(
+      const std::vector<std::pair<std::shared_ptr<PhotonCamera>,
+                                  frc::Transform3d>>& cameras) {
+    this->cameras = cameras;
+  }
+
+  /**
    * Get the Position Estimation Strategy being used by the Position Estimator.
    *
    * @return the strategy
