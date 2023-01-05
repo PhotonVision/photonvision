a6ba5d7fdcfe2c88c99ce6e515d70e95cc4570ee
Matt <matthew.morley.ca@gmail.com>
2023-01-05 09:09:59 -0800

    More cleanup

--- photon-core/src/main/java/org/photonvision/vision/aruco/PhotonArucoDetector.java
+++ photon-core/src/main/java/org/photonvision/vision/aruco/PhotonArucoDetector.java
@@ -94,15 +94,6 @@ public class PhotonArucoDetector {
                         cornerMat.get(0, 3)[1]
                     };
             cornerMat.release();
-            // // todo: only do pose est when 3d is enabled
-            // if (coeffs != null && xCorners[0] != 0) {
-            //     final var axis =
-            //             VecBuilder.fill(rvecs.get(i, 0)[0], rvecs.get(i, 0)[1], rvecs.get(i, 0)[2]);
-            //     translation =
-            //             new Translation3d(tvecs.get(i, 0)[0], tvecs.get(i, 0)[1], tvecs.get(i, 0)[2]);
-            // // 201
-            //     tagPose = new Pose3d(translation, new Rotation3d(axis, Core.norm(rvecs.col(i))));
-            // }
 
             toReturn[i] =
                     new ArucoDetectionResult(
