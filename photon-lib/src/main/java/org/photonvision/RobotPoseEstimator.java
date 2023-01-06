dbe7464ea9721fa30f54d6e778851b237ffeef63
Nick Hadley <nick.hadley31@gmail.com>
2023-01-06 17:41:47 -0500

    Change pose estimator to take robotToCamera (#698)
    Co-authored-by: Matt <matthew.morley.ca@gmail.com>

--- photon-lib/src/main/java/org/photonvision/RobotPoseEstimator.java
+++ photon-lib/src/main/java/org/photonvision/RobotPoseEstimator.java
@@ -73,14 +73,11 @@ public class RobotPoseEstimator {
     /**
      * Create a new RobotPoseEstimator.
      *
-     * <p>Example: {@code <code> <p> Map<Integer, Pose3d> map = new HashMap<>(); <p> map.put(1, new
-     * Pose3d(1.0, 2.0, 3.0, new Rotation3d())); // Tag ID 1 is at (1.0,2.0,3.0) </code> }
-     *
-     * @param aprilTags A AprilTagFieldLayout linking AprilTag IDs to Pose3ds with respect to the
-     *     FIRST field.
+     * @param aprilTags A WPILib {@link AprilTagFieldLayout} linking AprilTag IDs to Pose3ds with
+     *     respect to the FIRST field.
      * @param strategy The strategy it should use to determine the best pose.
      * @param cameras An ArrayList of Pairs of PhotonCameras and their respective Transform3ds from
-     *     the center of the robot to the cameras.
+     *     the center of the robot to the camera mount positions (ie, robot -> camera).
      */
     public RobotPoseEstimator(
             AprilTagFieldLayout aprilTags,
@@ -96,7 +93,7 @@ public class RobotPoseEstimator {
     /**
      * Update the estimated pose using the selected strategy.
      *
-     * @return The updated estimated pose and the latency in milliseconds Estimated pose may be null
+     * @return The updated estimated pose and the latency in milliseconds. Estimated pose may be null
      *     if no targets were seen
      */
     public Optional<Pair<Pose3d, Double>> update() {
