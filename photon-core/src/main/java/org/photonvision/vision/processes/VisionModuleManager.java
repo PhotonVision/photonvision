b0adea9be0bd799aa6a959d6744ee475d0fef515
Matt <matthew.morley.ca@gmail.com>
2023-03-01 10:27:23 -0500

    Fix camera index when attaching new camera

--- photon-core/src/main/java/org/photonvision/vision/processes/VisionModuleManager.java
+++ photon-core/src/main/java/org/photonvision/vision/processes/VisionModuleManager.java
@@ -20,8 +20,13 @@ package org.photonvision.vision.processes;
 import java.util.*;
 import java.util.stream.Collectors;
 
+import org.photonvision.common.logging.LogGroup;
+import org.photonvision.common.logging.Logger;
+
 /** VisionModuleManager has many VisionModules, and provides camera configuration data to them. */
 public class VisionModuleManager {
+    private final Logger logger = new Logger(VisionModuleManager.class, LogGroup.VisionModule);
+
     private static class ThreadSafeSingleton {
         private static final VisionModuleManager INSTANCE = new VisionModuleManager();
     }
@@ -77,8 +82,12 @@ public class VisionModuleManager {
         // The best we can do is try to avoid a case where the stream index runs away to infinity
         // since we can only stream 5 cameras at once
 
+        var bigList = new ArrayList<VisionSource>();
+        bigList.addAll(this.getModules().stream().map(it->it.visionSource).collect(Collectors.toList()));
+        bigList.addAll(config);
+
         for (var v : config) {
-            var listNoV = new ArrayList<>(config);
+            var listNoV = new ArrayList<>(bigList);
             listNoV.remove(v);
             if (listNoV.stream()
                     .anyMatch(
@@ -92,6 +101,7 @@ public class VisionModuleManager {
                         .contains(idx)) {
                     idx++;
                 }
+                logger.debug("Assigning idx " + idx);
                 v.getCameraConfiguration().streamIndex = idx;
             }
         }
