e954cc0b529d1b42a1e3f7891d93b0a75edf287a
StephenDay1 <57306301+StephenDay1@users.noreply.github.com>
2023-03-13 07:58:29 -0400

    Remove ErodeDilate from UI and ColoredShapePipeline

--- photon-core/src/main/java/org/photonvision/vision/pipeline/ColoredShapePipeline.java
+++ photon-core/src/main/java/org/photonvision/vision/pipeline/ColoredShapePipeline.java
@@ -34,7 +34,6 @@ import org.photonvision.vision.target.TrackedTarget;
 @SuppressWarnings({"DuplicatedCode"})
 public class ColoredShapePipeline
         extends CVPipeline<CVPipelineResult, ColoredShapePipelineSettings> {
-    private final ErodeDilatePipe erodeDilatePipe = new ErodeDilatePipe();
     private final SpeckleRejectPipe speckleRejectPipe = new SpeckleRejectPipe();
     private final FindContoursPipe findContoursPipe = new FindContoursPipe();
     private final FindPolygonPipe findPolygonPipe = new FindPolygonPipe();
@@ -71,11 +70,6 @@ public class ColoredShapePipeline
                         settings.offsetDualPointAArea,
                         settings.offsetDualPointB,
                         settings.offsetDualPointBArea);
-
-        ErodeDilatePipe.ErodeDilateParams erodeDilateParams =
-                new ErodeDilatePipe.ErodeDilateParams(settings.erode, settings.dilate, 5);
-        // TODO: add kernel size to pipeline settings
-        erodeDilatePipe.setParams(erodeDilateParams);
 
         SpeckleRejectPipe.SpeckleRejectParams speckleRejectParams =
                 new SpeckleRejectPipe.SpeckleRejectParams(settings.contourSpecklePercentage);
@@ -174,12 +168,6 @@ public class ColoredShapePipeline
     @Override
     protected CVPipelineResult process(Frame frame, ColoredShapePipelineSettings settings) {
         long sumPipeNanosElapsed = 0L;
-
-        //        var erodeDilateResult = erodeDilatePipe.run(rawInputMat);
-        //        sumPipeNanosElapsed += erodeDilateResult.nanosElapsed;
-        //
-        //        CVPipeResult<Mat> hsvPipeResult = hsvPipe.run(rawInputMat);
-        //        sumPipeNanosElapsed += hsvPipeResult.nanosElapsed;
 
         CVPipeResult<List<Contour>> findContoursResult =
                 findContoursPipe.run(frame.processedImage.getMat());
