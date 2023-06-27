6e6a3ed0843171c7cb7116fd0eda0ae61fd7a05b
Sriman Achanta <68172138+srimanachanta@users.noreply.github.com>
2023-06-27 17:18:58 -0400

    Explicitly state vuetify dark and light themes
    Good memory usage who?

--- photon-client/src/plugins/vuetify.ts
+++ photon-client/src/plugins/vuetify.ts
@@ -2,21 +2,38 @@ import Vue from "vue";
 import Vuetify from "vuetify";
 import "vuetify/dist/vuetify.min.css";
 import "@mdi/font/css/materialdesignicons.css";
+import type {VuetifyThemeVariant} from "vuetify/types/services/theme";
 
 Vue.use(Vuetify);
 
-const theme = Object.freeze({
+const darkTheme: VuetifyThemeVariant = Object.freeze({
     primary: "#006492",
     secondary: "#39A4D5",
     accent: "#FFD843",
-    background: "#232C37"
+    background: "#232C37",
+    error: "#B00020",
+    info: "#2196F3",
+    success: "#4CAF50",
+    warning: "#FB8C00"
+});
+
+const lightTheme: VuetifyThemeVariant = Object.freeze({
+    primary: "#006492",
+    secondary: "#39A4D5",
+    accent: "#FFD843",
+    background: "#232C37",
+    error: "#B00020",
+    info: "#2196F3",
+    success: "#4CAF50",
+    warning: "#FB8C00"
+
 });
 
 export default new Vuetify({
     theme: {
         themes: {
-            light: theme,
-            dark: theme
+            light: lightTheme,
+            dark: darkTheme
         }
     },
     breakpoint: {
