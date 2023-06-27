768b27bb732d73e9452d0d8232939db4e1f1d20a
Sriman Achanta <68172138+srimanachanta@users.noreply.github.com>
2023-06-27 17:16:35 -0400

    Add background styling

--- photon-client/src/App.vue
+++ photon-client/src/App.vue
@@ -3,90 +3,57 @@ import HelloWorld from "./components/HelloWorld.vue";
 </script>
 
 <template>
-  <div id="app">
-    <header>
-      <img
-        alt="Vue logo"
-        class="logo"
-        src="assets/images/logo.svg"
-        width="125"
-        height="125"
-      />
+  <v-app>
+    <photon-sidebar/>
+    <v-main>
+      <v-container
+          fluid
+          fill-height
+      >
+        <v-layout>
+          <v-flex>
+            <router-view @switch-to-cameras="() => {}" />
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </v-main>
 
-      <div class="wrapper">
-        <HelloWorld msg="You did it!" />
-
-        <nav>
-          <router-link to="/">Home</router-link>
-          <router-link to="/about">About</router-link>
-        </nav>
-      </div>
-    </header>
-
-    <router-view />
-  </div>
+    <photon-log-view/>
+  </v-app>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+<style lang="scss">
+  ::-webkit-scrollbar {
+    width: 0.5em;
+    border-radius: 5px;
   }
 
-  .logo {
-    margin: 0 2rem 0 0;
+  ::-webkit-scrollbar-track {
+    -webkit-box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.3);
+    border-radius: 10px;
   }
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
+  ::-webkit-scrollbar-thumb {
+    background-color: #ffd843;
+    border-radius: 10px;
   }
 
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
+  .container {
+    background-color: #232c37;
+    padding: 0 !important;
+  }
 
-    padding: 1rem 0;
-    margin-top: 1rem;
+  #title {
+    color: #ffd843;
+  }
+</style>
+
+<style lang="scss">
+@import 'vuetify/src/styles/settings/_variables';
+
+@media #{map-get($display-breakpoints, 'md-and-down')} {
+  html {
+    font-size: 14px !important;
   }
 }
 </style>
