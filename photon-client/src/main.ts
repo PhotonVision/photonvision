da8f923ac65cbac6fd9ac344fe62d4a801124e53
Sriman Achanta <68172138+srimanachanta@users.noreply.github.com>
2023-06-30 23:07:52 -0400

    Add axios base URL cause i am lazy

--- photon-client/src/main.ts
+++ photon-client/src/main.ts
@@ -4,16 +4,22 @@ import App from "@/App.vue";
 import { createPinia, PiniaVuePlugin } from "pinia";
 import router from "@/router";
 import vuetify from "@/plugins/vuetify";
+import axios from "axios";
 
 // Handle Plugins
 Vue.use(PiniaVuePlugin);
+
+const backendAddress = process.env.NODE_ENV === "production"
+    ? location.host
+    : location.hostname + ":5800";
+axios.defaults.baseURL = `http://${backendAddress}/api`;
 
 new Vue({
   router,
   vuetify,
   pinia: createPinia(),
   provide: {
-    backendAddress: process.env.NODE_ENV === "production" ? location.host : location.hostname + ":5800"
+    backendAddress
   },
   render: (h) => h(App)
 }).$mount("#app");
