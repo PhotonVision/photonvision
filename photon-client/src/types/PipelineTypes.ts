7556ffa987150390271a63a31cebc036a5419bcf
Sriman Achanta <68172138+srimanachanta@users.noreply.github.com>
2023-07-05 18:38:16 -0400

    Add default pipeline settings exports

--- photon-client/src/types/PipelineTypes.ts
+++ photon-client/src/types/PipelineTypes.ts
@@ -1,7 +1,14 @@
 enum PipelineType {
+    Unknown=-1,
     Reflective=2,
     Colored=3,
     AprilTag=4
+}
+
+enum AprilTagFamily {
+    Family36h11=0,
+    Family25h9=1,
+    Family16h5=2
 }
 
 // driver mode index -1
@@ -56,12 +63,71 @@ export interface PipelineSettings {
 }
 // Omitted things are based on previous usage and I have no clue whether they should or shouldn't be
 export type ConfigurablePipelineSettings = Partial<Omit<PipelineSettings, "offsetDualPointAArea" | "cornerDetectionSideCount" | "pipelineNickname" | "pipelineIndex" | "cornerDetectionUseConvexHulls" | "offsetDualPointA" | "offsetDualPointB" | "ledMode" | "offsetSinglePoint" | "offsetDualPointBArea" | "cornerDetectionExactSideCount" | "cornerDetectionStrategy">>
+export const DefaultPipelineSettings: PipelineSettings = {
+    offsetRobotOffsetMode: 0,
+    streamingFrameDivisor: 0,
+    offsetDualPointBArea: 0,
+    contourGroupingMode: 0,
+    hsvValue: {"first": 50, "second": 255},
+    cameraBlueGain: 20,
+    cameraRedGain: 11,
+    cornerDetectionSideCount: 4,
+    contourRatio: {"first": 0, "second": 20},
+    contourTargetOffsetPointEdge: 0,
+    pipelineNickname: "New Pipeline",
+    inputImageRotationMode: 0,
+    contourArea: {"first": 0, "second": 100},
+    solvePNPEnabled: false,
+    contourFullness: {"first": 0, "second": 100},
+    pipelineIndex: 0,
+    inputShouldShow: false,
+    cameraAutoExposure: false,
+    contourSpecklePercentage: 5,
+    contourTargetOrientation: 1,
+    cornerDetectionUseConvexHulls: true,
+    outputShouldShow: true,
+    outputShouldDraw: true,
+    offsetDualPointA: {"x": 0, "y": 0},
+    offsetDualPointB: {"x": 0, "y": 0},
+    hsvHue: {"first": 50, "second": 180},
+    hueInverted: false,
+    contourSortMode: 0,
+    offsetSinglePoint: {"x": 0, "y": 0},
+    cameraBrightness: 50,
+    offsetDualPointAArea: 0,
+    cornerDetectionExactSideCount: false,
+    cameraVideoModeIndex: 0,
+    cornerDetectionStrategy: 0,
+    cornerDetectionAccuracyPercentage: 10,
+    hsvSaturation: {"first": 50, "second": 255},
+    contourIntersection: 1,
+
+    // These settings will be overridden by different pipeline types
+    cameraGain: -1,
+    targetModel: -1,
+    ledMode: false,
+    outputShowMultipleTargets: false,
+    cameraExposure: -1,
+    pipelineType: PipelineType.Unknown
+};
 
 export interface ReflectivePipelineSettings extends PipelineSettings {
     contourFilterRangeY: number
     contourFilterRangeX: number
 }
 export type ConfigurableReflectivePipelineSettings = Partial<ReflectivePipelineSettings> & ConfigurablePipelineSettings
+export const DefaultReflectivePipelineSettings: ReflectivePipelineSettings = {
+    ...DefaultPipelineSettings,
+    cameraGain: 20,
+    targetModel: 0,
+    ledMode: true,
+    outputShowMultipleTargets: false,
+    cameraExposure: 6,
+    pipelineType: PipelineType.Reflective,
+
+    contourFilterRangeY: 2,
+    contourFilterRangeX: 2
+};
 
 export interface ColoredShapePipelineSettings extends PipelineSettings {
     erode: boolean
@@ -78,6 +144,27 @@ export interface ColoredShapePipelineSettings extends PipelineSettings {
 }
 // Omitted things are based on previous usage and I have no clue whether they should or shouldn't be
 export type ConfigurableColoredShapePipelineSettings = Partial<Omit<ColoredShapePipelineSettings, "erode" | "cameraCalibration" | "dilate" | "circleAccuracy" | "minDist" >> & ConfigurablePipelineSettings
+export const DefaultColoredShapePipelineSettings: ColoredShapePipelineSettings = {
+    ...DefaultPipelineSettings,
+    cameraGain: 75,
+    targetModel: 0,
+    ledMode: true,
+    outputShowMultipleTargets: false,
+    cameraExposure: 20,
+    pipelineType: PipelineType.Colored,
+
+    erode: false,
+    cameraCalibration: null,
+    dilate: false,
+    circleAccuracy: 20,
+    contourRadius: {"first": 0, "second": 100},
+    circleDetectThreshold: 5,
+    accuracyPercentage: 10,
+    contourShape: 2,
+    contourPerimeter: {"first": 0, "second": 1.7976931348623157e+308},
+    minDist: 20,
+    maxCannyThresh: 90
+};
 
 export interface AprilTagPipelineSettings extends PipelineSettings {
     hammingDist: number
@@ -88,7 +175,26 @@ export interface AprilTagPipelineSettings extends PipelineSettings {
     refineEdges: boolean
     debug: boolean
     threads: number
-    tagFamily: number
+    tagFamily: AprilTagFamily
 }
 // Omitted things are based on previous usage and I have no clue whether they should or shouldn't be
 export type ConfigurableAprilTagPipelineSettings = Partial<Omit<AprilTagPipelineSettings, "hammingDist" | "debug">> & ConfigurablePipelineSettings
+export const DefaultAprilTagPipelineSettings: AprilTagPipelineSettings = {
+    ...DefaultPipelineSettings,
+    cameraGain: 75,
+    targetModel: 8,
+    ledMode: false,
+    outputShowMultipleTargets: true,
+    cameraExposure: 20,
+    pipelineType: PipelineType.AprilTag,
+
+    hammingDist: 0,
+    numIterations: 40,
+    decimate: 1,
+    blur: 0,
+    decisionMargin: 35,
+    refineEdges: true,
+    debug: false,
+    threads: 4,
+    tagFamily: AprilTagFamily.Family16h5
+};
