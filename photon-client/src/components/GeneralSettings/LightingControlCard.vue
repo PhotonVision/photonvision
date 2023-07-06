7c77f68b6c88990df46f6dc5fc42c5dcbfbf6522
Sriman Achanta <68172138+srimanachanta@users.noreply.github.com>
2023-07-06 04:44:27 -0400

    Create LightingControlCard.vue

---
+++ photon-client/src/components/GeneralSettings/LightingControlCard.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import CvSlider from "@/components/common/cv-slider.vue";
+import {useSettingsStore} from "@/stores/settings/GeneralSettingsStore";
+</script>
+
+<template>
+  <v-card
+      dark
+      class="mb-3 pr-6 pb-3"
+      style="background-color: #006492;"
+  >
+    <v-card-title>Lighting Control</v-card-title>
+    <div class="ml-5">
+      <cv-slider
+          v-model="useSettingsStore().lighting.brightness"
+          label="Brightness"
+          class="pt-2"
+          :slider-cols="12"
+          :min="0"
+          :max="100"
+          @input="args => useSettingsStore().changeLEDBrightness(args)"
+      />
+    </div>
+  </v-card>
+</template>
