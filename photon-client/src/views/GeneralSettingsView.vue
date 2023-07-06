465b04a86bf5ba2635fa28f508fc9a1a70f3643e
Sriman Achanta <68172138+srimanachanta@users.noreply.github.com>
2023-07-06 16:26:21 -0400

    Update GeneralSettingsView.vue

--- photon-client/src/views/GeneralSettingsView.vue
+++ photon-client/src/views/GeneralSettingsView.vue
@@ -1,5 +1,26 @@
+<script setup lang="ts">
+import MetricsCard from "@/components/GeneralSettings/MetricsCard.vue";
+import DeviceControlCard from "@/components/GeneralSettings/DeviceControlCard.vue";
+import NetworkingCard from "@/components/GeneralSettings/NetworkingCard.vue";
+import LightingControlCard from "@/components/GeneralSettings/LightingControlCard.vue";
+import {useSettingsStore} from "@/stores/settings/GeneralSettingsStore";
+
+const settingsStore = useSettingsStore();
+</script>
+
 <template>
-  <div>
-    Hello From the General Settings View
-  </div>
+  <v-row
+      class="pa-3"
+      no-gutters
+  >
+    <v-col
+        cols="12"
+        style="max-width: 1400px"
+    >
+      <MetricsCard/>
+      <DeviceControlCard/>
+      <NetworkingCard/>
+      <LightingControlCard v-if="settingsStore.lighting.supported"/>
+    </v-col>
+  </v-row>
 </template>
