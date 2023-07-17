1dfad6f756afdb293f6175a920eeefdbc63bf253
Sriman Achanta <68172138+srimanachanta@users.noreply.github.com>
2023-07-17 10:45:33 -0400

    Update LogTypes

--- photon-client/src/types/SettingTypes.ts
+++ photon-client/src/types/SettingTypes.ts
@@ -43,10 +43,11 @@ export interface LightingSettings {
 }
 
 export enum LogLevel {
-    ERROR,
-    WARN,
-    INFO,
-    DEBUG
+    ERROR=0,
+    WARN=1,
+    INFO=2,
+    DEBUG=3,
+    TRACE=4
 }
 
 export interface LogMessage {
