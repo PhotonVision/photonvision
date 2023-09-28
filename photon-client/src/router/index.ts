d7b105effa90c390e79e0fd4fd20de404ac83537
Sriman Achanta <68172138+srimanachanta@users.noreply.github.com>
2023-09-27 22:44:31 -0400

    add doc

--- photon-client/src/router/index.ts
+++ photon-client/src/router/index.ts
@@ -10,6 +10,7 @@ import NotFoundView from "@/views/NotFoundView.vue";
 Vue.use(VueRouter);
 
 const router = new VueRouter({
+  // Using HTML5 History Mode is problematic with Javalin because each route is treated as a server endpoint which causes Javalin to return a 404 error before being redirected to the UI.
   // mode: "history",
   base: import.meta.env.BASE_URL,
   routes: [
