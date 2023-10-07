d7786eaa38730f46a2cd32aee315fb88ab985b19
amquake <noleetarrr@gmail.com>
2023-10-06 19:48:34 -0700

    use known tags

--- photon-lib/src/main/java/org/photonvision/estimation/VisionEstimation.java
+++ photon-lib/src/main/java/org/photonvision/estimation/VisionEstimation.java
@@ -103,7 +103,7 @@ public class VisionEstimation {
         Point[] points = OpenCVHelp.cornersToPoints(corners);
 
         // single-tag pnp
-        if (visTags.size() == 1) {
+        if (knownTags.size() == 1) {
             var camToTag =
                     OpenCVHelp.solvePNP_SQUARE(
                             cameraMatrix, distCoeffs, TargetModel.kTag16h5.vertices, points);
