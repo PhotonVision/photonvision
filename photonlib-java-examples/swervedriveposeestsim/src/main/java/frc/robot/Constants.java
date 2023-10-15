df442d6ae68157dbdeba71baf27fc10c49022a7c
Matthew Morley <matthew.morley.ca@gmail.com>
2023-10-15 10:21:47 -0400

    Update Constants.java

--- photonlib-java-examples/swervedriveposeestsim/src/main/java/frc/robot/Constants.java
+++ photonlib-java-examples/swervedriveposeestsim/src/main/java/frc/robot/Constants.java
@@ -36,7 +36,6 @@ import edu.wpi.first.math.geometry.Translation3d;
 import edu.wpi.first.math.numbers.N1;
 import edu.wpi.first.math.numbers.N3;
 import edu.wpi.first.math.util.Units;
-import java.io.IOException;
 
 public class Constants {
     public static class Vision {
@@ -46,7 +45,8 @@ public class Constants {
                 new Transform3d(new Translation3d(0.5, 0.0, 0.5), new Rotation3d(0, 0, 0));
 
         // The layout of the AprilTags on the field
-        public static final AprilTagFieldLayout kTagLayout = AprilTagFields.kDefaultField.loadAprilTagLayoutField();
+        public static final AprilTagFieldLayout kTagLayout =
+                AprilTagFields.kDefaultField.loadAprilTagLayoutField();
 
         // The standard deviations of our vision estimated poses, which affect correction rate
         // (Fake values. Experiment and determine estimation noise on an actual robot.)
