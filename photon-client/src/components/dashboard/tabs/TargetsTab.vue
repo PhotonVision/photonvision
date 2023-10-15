75bf4aed712b649837b5a2084a4ca0980069fcdf
amquake <noleetarrr@gmail.com>
2023-10-14 20:25:36 -0700

    hide multitag ambiguity

--- photon-client/src/components/dashboard/tabs/TargetsTab.vue
+++ photon-client/src/components/dashboard/tabs/TargetsTab.vue
@@ -69,7 +69,7 @@ import { useStateStore } from "@/stores/StateStore";
                   useCameraSettingsStore().currentPipelineSettings.solvePNPEnabled
                 "
               >
-                <td>{{ target.ambiguity?.toFixed(2) }}%</td>
+                <td>{{ target.ambiguity >= 0 ? target.ambiguity?.toFixed(2)+"%" : "(In Multi-Target)" }}</td>
               </template>
             </tr>
           </tbody>
