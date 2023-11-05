46bf502e0ff89c6e3cfd66d931d120a9ad85b383
Technologyman00 <collinost@gmail.com>
2023-11-05 12:19:36 -0600

    Added Pi 5 enum to PiVersion

--- photon-core/src/main/java/org/photonvision/common/hardware/PiVersion.java
+++ photon-core/src/main/java/org/photonvision/common/hardware/PiVersion.java
@@ -27,6 +27,7 @@ public enum PiVersion {
     ZERO_2_W("Raspberry Pi Zero 2"),
     PI_3("Pi 3"),
     PI_4("Pi 4"),
+	PI_5("Pi 5"),
     COMPUTE_MODULE_3("Compute Module 3"),
     UNKNOWN("UNKNOWN");
 
