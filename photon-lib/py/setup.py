4be9004564df42462012e543088145447e9cd52f
Chris Gerth <chrisgerth010592@gmail.com>
2023-12-10 19:55:12 -0600

    my cat is fuzzy

--- photon-lib/py/setup.py
+++ photon-lib/py/setup.py
@@ -7,11 +7,11 @@ print(f"Building version {versionStr}")
 setup(
     name='photonlibpy',
     packages=find_packages(),
-    version=versionStr,
+    version="0.0.2",
     install_requires=[
         "wpilib<2025,>=2024.0.0b2", 
     ],
-    description="Pure-python implementation of PhotonLib for interfacing with PhotonVision on coprocessors",
+    description=f"Pure-python implementation of PhotonLib for interfacing with PhotonVision on coprocessors. Goes with photonvision version {versionStr}",
     url="https://photonvision.org",
     author="Photonvision Development Team",
 )
