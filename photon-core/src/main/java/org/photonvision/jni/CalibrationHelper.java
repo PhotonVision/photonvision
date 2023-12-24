22a4f5b79f7ac3e339d53f1b4f8ac65de32217a2
Matt <matthew.morley.ca@gmail.com>
2023-12-24 13:21:48 -0800

    Working JNI example

--- photon-core/src/main/java/org/photonvision/jni/CalibrationHelper.java
+++ photon-core/src/main/java/org/photonvision/jni/CalibrationHelper.java
@@ -8,4 +8,9 @@ public class CalibrationHelper {
     public static native long Create(int width, int height, long overlayMatPtr, double tolerance);
     public static native long Destroy();
     public static native CalResult Detect(long inputImg, long outputImg);
+
+    public static void main(String[] args) {
+        System.load("/home/matt/Documents/GitHub/photonvision/photon-core/build/libs/photoncoreJNI/shared/linuxx86-64/release/libphotoncorejni.so");
+        System.out.println(Create(1, 2, 3, 4));
+    }
 }
---
+++ photon-core/src/main/native/jni/CalibrationHelperJni.cpp
@@ -0,0 +1,10 @@
+#include <org_photonvision_jni_CalibrationHelper.h>
+
+extern "C" {
+
+JNIEXPORT jlong JNICALL Java_org_photonvision_jni_CalibrationHelper_Create
+  (JNIEnv *, jclass, jint, jint, jlong, jdouble) {
+    return 0;
+}
+
+}
