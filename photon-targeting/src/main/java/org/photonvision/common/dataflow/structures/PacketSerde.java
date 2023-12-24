c0c79aa44429299ac3dfe304f34a2f625cfffdd7
Sriman Achanta <68172138+srimanachanta@users.noreply.github.com>
2023-12-24 10:10:46 -0500

    Update PacketSerde.java

--- photon-targeting/src/main/java/org/photonvision/common/dataflow/structures/PacketSerde.java
+++ photon-targeting/src/main/java/org/photonvision/common/dataflow/structures/PacketSerde.java
@@ -18,7 +18,7 @@
 package org.photonvision.common.dataflow.structures;
 
 public interface PacketSerde<T> {
-    public int getMaxByteSize();
+    int getMaxByteSize();
 
     void pack(Packet packet, T value);
 
