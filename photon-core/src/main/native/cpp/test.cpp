804099360e274220fcdf0b68701e5b6240a419f1
Matt <matthew.morley.ca@gmail.com>
2023-12-23 18:02:15 -0800

    Run lint

--- photon-core/src/main/native/cpp/test.cpp
+++ photon-core/src/main/native/cpp/test.cpp
@@ -1,16 +1,34 @@
+/*
+ * Copyright (C) Photon Vision.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 #include "test.h"
+
+#include <cstdio>
 
 #include <opencv2/core.hpp>
 #include <opencv2/core/mat.hpp>
 #include <opencv2/imgcodecs.hpp>
 
-#include <stdio.h>
-
 int some_test() {
-    
-    cv::Mat mat = cv::imread("/home/matt/Documents/GitHub/photonvision/test-resources/testimages/2022/WPI/FarLaunchpad13ft10in.png");
+  cv::Mat mat = cv::imread(
+      "/home/matt/Documents/GitHub/photonvision/test-resources/testimages/2022/"
+      "WPI/FarLaunchpad13ft10in.png");
 
-    printf("mat size %i %i\n", mat.rows, mat.cols);
+  std::printf("mat size %i %i\n", mat.rows, mat.cols);
 
-    return 1;
+  return 1;
 }
