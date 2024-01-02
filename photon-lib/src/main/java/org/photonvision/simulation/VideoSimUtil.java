26b38f5bc1caf77207085fc1d44282422709f6e7
Drew Williams <williams.r.drew@gmail.com>
2024-01-02 09:19:56 -0500

    fixes potential use after free with apriltag image

--- photon-lib/src/main/java/org/photonvision/simulation/VideoSimUtil.java
+++ photon-lib/src/main/java/org/photonvision/simulation/VideoSimUtil.java
@@ -101,7 +101,8 @@ public class VideoSimUtil {
      */
     public static Mat get36h11TagImage(int id) {
         RawFrame frame = AprilTag.generate36h11AprilTagImage(id);
-        Mat result = new Mat(10, 10, CvType.CV_8UC1, frame.getData(), frame.getStride());
+        Mat result = new Mat(10, 10, CvType.CV_8UC1, frame.getData(), frame.getStride()).clone();
+        frame.close();
         return result;
     }
 
