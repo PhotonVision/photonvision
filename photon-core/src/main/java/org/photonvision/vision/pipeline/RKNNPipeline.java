c7f0e40284175fc42d07381b5eb78ec184587a59
laviRZ <laviarzi@gmail.com>
2024-01-31 01:31:27 +0200

    calculate fps when there are no detections

--- photon-core/src/main/java/org/photonvision/vision/pipeline/RKNNPipeline.java
+++ photon-core/src/main/java/org/photonvision/vision/pipeline/RKNNPipeline.java
@@ -79,7 +79,7 @@ public class RKNNPipeline extends CVPipeline<CVPipelineResult, RKNNPipelineSetti
         SortContoursPipe.SortContoursParams sortContoursParams =
                 new SortContoursPipe.SortContoursParams(
                         settings.contourSortMode,
-                        settings.outputShowMultipleTargets ? MAX_MULTI_TARGET_RESULTS : 1,
+                        settings.outputShowMultipleTargets ? 999 : 1,
                         frameStaticProperties);
         sortContoursPipe.setParams(sortContoursParams);
 
@@ -113,7 +113,7 @@ public class RKNNPipeline extends CVPipeline<CVPipelineResult, RKNNPipelineSetti
         CVPipeResult<List<NeuralNetworkPipeResult>> rknnResult = rknnPipe.run(input_frame.colorImage);
         sumPipeNanosElapsed += rknnResult.nanosElapsed;
 
-        if (rknnResult.output.size() == 0) return new CVPipelineResult(0, 0, List.of(), input_frame);
+        if (rknnResult.output.size() == 0) return new CVPipelineResult(0, calculateFPSPipe.run(null).output, List.of(), input_frame);
 
         var filterContoursResult = filterContoursPipe.run(rknnResult.output);
         sumPipeNanosElapsed += filterContoursResult.nanosElapsed;
@@ -129,52 +129,49 @@ public class RKNNPipeline extends CVPipeline<CVPipelineResult, RKNNPipelineSetti
                 collect2dTargetsPipe.run(sortContoursResult.output);
         sumPipeNanosElapsed += collect2dTargetsResult.nanosElapsed;
 
-        Size size = null;
-        switch (settings.streamingFrameDivisor) {
-            case NONE:
-                break;
-            case HALF:
-                size = new Size(processed.width() / 2, processed.height() / 2);
-                break;
-            case QUARTER:
-                size = new Size(processed.width() / 4, processed.height() / 4);
-                break;
-            case SIXTH:
-                size = new Size(processed.width() / 6, processed.height() / 6);
-                break;
-            default:
-                break;
-        }
+        // Size size = null;
+        // switch (settings.streamingFrameDivisor) {
+        //     case NONE:
+        //         break;
+        //     case HALF:
+        //         size = new Size(processed.width() / 2, processed.height() / 2);
+        //         break;
+        //     case QUARTER:
+        //         size = new Size(processed.width() / 4, processed.height() / 4);
+        //         break;
+        //     case SIXTH:
+        //         size = new Size(processed.width() / 6, processed.height() / 6);
+        //         break;
+        //     default:
+        //         break;
+        // }
 
-        if (settings.outputShouldShow) {
-            // for (var target : rknnResult.output) {
-            // Imgproc.rectangle(
-            // processed,
-            // new Point(target.box.x, target.box.y),
-            // new Point(target.box.x + target.box.width, target.box.y + target.box.height),
-            // isIn(filterContoursResult.output, target) ? new Scalar(0, 255, 0) : new
-            // Scalar(0, 0, 255),
-            // 2 * processed.width() / 640);
-            // var label = String.format("%s (%f)", target.classIdx, target.confidence);
-            // Imgproc.putText(
-            // processed,
-            // label,
-            // new Point(target.box.x, target.box.y + 12),
-            // 0,
-            // 0.6 * processed.width() / 640,
-            // ColorHelper.colorToScalar(java.awt.Color.white),
-            // 2 * processed.width() / 640);
-            // }
-            // Imgproc.resize(processed, processed, size);
-        }
-        // Imgproc.resize(input_frame.colorImage.getMat(),
-        // input_frame.colorImage.getMat(), size);
-
-        var fpsResult = calculateFPSPipe.run(null);
-        var fps = fpsResult.output;
+        // if (settings.outputShouldShow) {
+        //     // for (var target : rknnResult.output) {
+        //     // Imgproc.rectangle(
+        //     // processed,
+        //     // new Point(target.box.x, target.box.y),
+        //     // new Point(target.box.x + target.box.width, target.box.y + target.box.height),
+        //     // isIn(filterContoursResult.output, target) ? new Scalar(0, 255, 0) : new
+        //     // Scalar(0, 0, 255),
+        //     // 2 * processed.width() / 640);
+        //     // var label = String.format("%s (%f)", target.classIdx, target.confidence);
+        //     // Imgproc.putText(
+        //     // processed,
+        //     // label,
+        //     // new Point(target.box.x, target.box.y + 12),
+        //     // 0,
+        //     // 0.6 * processed.width() / 640,
+        //     // ColorHelper.colorToScalar(java.awt.Color.white),
+        //     // 2 * processed.width() / 640);
+        //     // }
+        //     // Imgproc.resize(processed, processed, size);
+        // }
+        // // Imgproc.resize(input_frame.colorImage.getMat(),
+        // // input_frame.colorImage.getMat(), size);
 
         return new CVPipelineResult(
-                (System.nanoTime() - timeStarted) * 1e-6, fps, collect2dTargetsResult.output, input_frame);
+                (System.nanoTime() - timeStarted) * 1e-6,  calculateFPSPipe.run(null).output, collect2dTargetsResult.output, input_frame);
     }
 
     boolean isIn(List<NeuralNetworkPipeResult> list, NeuralNetworkPipeResult target) {
