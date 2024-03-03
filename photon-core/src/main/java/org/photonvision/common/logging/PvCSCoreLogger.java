34d8d1b8065c9ba7b38cc44a161777facbd00f69
Matt M <matthew.morley.ca@gmail.com>
2024-03-02 20:23:35 -0500

    lint

--- photon-core/src/main/java/org/photonvision/common/logging/PvCSCoreLogger.java
+++ photon-core/src/main/java/org/photonvision/common/logging/PvCSCoreLogger.java
@@ -1,13 +1,27 @@
+/*
+ * Copyright (C) Photon Vision.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package org.photonvision.common.logging;
 
-import java.io.File;
-import java.nio.file.Path;
-
 import edu.wpi.first.cscore.CameraServerJNI;
+import java.nio.file.Path;
 
 /** Redirect cscore logs to our logger */
 public class PvCSCoreLogger {
-
     private static PvCSCoreLogger INSTANCE;
 
     public static PvCSCoreLogger getInstance() {
@@ -50,6 +64,7 @@ public class PvCSCoreLogger {
             levelmsg = "DEBUG";
             pvlevel = LogLevel.DEBUG;
         }
-        logger.log("CS: " + levelmsg + " " + level + ": " + msg + " (" + file + ":" + line + ")", pvlevel);
+        logger.log(
+                "CS: " + levelmsg + " " + level + ": " + msg + " (" + file + ":" + line + ")", pvlevel);
     }
 }
