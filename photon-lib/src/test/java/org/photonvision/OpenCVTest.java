5b17e4b65bdec0c923cddab13e4b79519b895da9
Matt <matthew.morley.ca@gmail.com>
2024-06-09 16:47:09 -0700

    Maybe fix test

--- photon-lib/src/test/java/org/photonvision/OpenCVTest.java
+++ photon-lib/src/test/java/org/photonvision/OpenCVTest.java
@@ -34,10 +34,13 @@ import edu.wpi.first.math.geometry.Rotation3d;
 import edu.wpi.first.math.geometry.Transform3d;
 import edu.wpi.first.math.geometry.Translation3d;
 import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.networktables.NetworkTablesJNI;
+import edu.wpi.first.util.CombinedRuntimeLoader;
 import java.io.IOException;
 import java.util.List;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.opencv.core.Core;
 import org.photonvision.estimation.CameraTargetRelation;
 import org.photonvision.estimation.OpenCVHelp;
 import org.photonvision.estimation.RotTrlTransform3d;
@@ -77,7 +80,17 @@ public class OpenCVTest {
 
     @BeforeAll
     public static void setUp() throws IOException {
-        OpenCvLoader.forceLoad();
+        OpenCvLoader.Helper.setExtractOnStaticLoad(false);
+        NetworkTablesJNI.Helper.setExtractOnStaticLoad(false);
+
+        try {
+            CombinedRuntimeLoader.loadLibraries(
+                    OpenCVTest.class, "wpiutiljni", "wpinetjni", "ntcorejni", Core.NATIVE_LIBRARY_NAME);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw e;
+        }
 
         // NT live for debug purposes
         NetworkTableInstance.getDefault().startServer();
