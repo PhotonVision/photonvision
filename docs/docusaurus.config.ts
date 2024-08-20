7e7e7ba5ccf4f4def74305134c0ba1f7677c752a
GrahamSH <grahamshllk@gmail.com>
2024-08-19 20:19:05 -0400

    change base for readthedocs

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -11,7 +11,9 @@ const config: Config = {
   url: "https://your-docusaurus-site.example.com",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/",
+  baseUrl: process.env.READTHEDOCS
+    ? `/${process.env.READTHEDOCS_LANGUAGE}/${process.env.READTHEDOCS_VERSION}/`
+    : "/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
