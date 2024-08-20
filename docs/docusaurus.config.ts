406938905a27cc300305835d8af2c406c02dd885
GrahamSH <grahamshllk@gmail.com>
2024-08-19 20:10:19 -0400

    fix links

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -93,7 +93,7 @@ const config: Config = {
           items: [
             {
               label: "Tutorial",
-              to: "/docs/intro",
+              to: "/docs/description",
             },
           ],
         },
