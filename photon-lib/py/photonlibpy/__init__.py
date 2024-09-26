fd1a430142267fd22fecdb632708848713b7d825
Matt Morley <matthew.morley.ca@gmail.com>
2024-09-25 22:37:55 -0700

    Clean up gradle cruft

--- photon-lib/py/photonlibpy/__init__.py
+++ photon-lib/py/photonlibpy/__init__.py
@@ -30,8 +30,9 @@ import hal
 import wpilib.cameraserver
 import robotpy_apriltag
 
-# and now our extension module
-import os
+import platform
+if platform.system().lower() == "Windows":
+    import os
+    os.add_dll_directory(os.path.dirname(os.path.realpath(__file__)))
 
-os.add_dll_directory(os.path.dirname(os.path.realpath(__file__)))
 from ._photonlibpy import *
