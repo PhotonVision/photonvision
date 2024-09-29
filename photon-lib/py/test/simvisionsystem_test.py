5b02c50c7173a1d15761c9c5bf4d0cb3dd1c92c3
Matt M <matthew.morley.ca@gmail.com>
2024-09-29 15:12:27 -0700

    yeet

--- photon-lib/py/test/simvisionsystem_test.py
+++ photon-lib/py/test/simvisionsystem_test.py
@@ -1,6 +1,7 @@
 import time
 import photonlibpy as pp
 from wpimath.geometry import *
+from robotpy_apriltag import *
 
 camera = pp.PhotonCamera("camera")
 
@@ -9,15 +10,14 @@ simcam.EnableDrawWireframe(True)
 
 simsystem = pp.VisionSystemSim("Test")
 
-simsystem.AddCamera(simcam, Transform3d())
-simsystem.AddVisionTargets(
-    "apriltag", [pp.VisionTargetSim(Pose3d(), pp.TargetModel.kAprilTag36h11)]
-)
+simsystem.AddCamera(simcam, Transform3d(0, 0, 1, Rotation3d()))
+
+simsystem.AddAprilTags(AprilTagFieldLayout.loadField(AprilTagField.k2024Crescendo))
 
 start = time.time()
 while True:
     simsystem.Update(Pose3d(
-        Translation3d(2, 0, 0),
+        Translation3d(8, 6, 0),
         Rotation3d(0, 0, time.time() - start)
     ))
     time.sleep(0.02)
