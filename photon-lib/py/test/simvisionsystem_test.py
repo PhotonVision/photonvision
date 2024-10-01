a0ef9a2fd9e603e003d37421e209521b2a9118a0
Matt Morley <matthew.morley.ca@gmail.com>
2024-09-30 20:45:19 -0700

    add sim test

--- photon-lib/py/test/simvisionsystem_test.py
+++ photon-lib/py/test/simvisionsystem_test.py
@@ -3,20 +3,21 @@ import photonlibpy as pp
 from wpimath.geometry import *
 from robotpy_apriltag import *
 
-camera = pp.PhotonCamera("camera")
+def test_simulation():
+    camera = pp.PhotonCamera("camera")
 
-simcam = pp.PhotonCameraSim(camera)
-simcam.EnableDrawWireframe(True)
+    simcam = pp.PhotonCameraSim(camera)
+    simcam.EnableDrawWireframe(True)
 
-simsystem = pp.VisionSystemSim("Test")
+    simsystem = pp.VisionSystemSim("Test")
 
-simsystem.AddCamera(simcam, Transform3d(0, 0, 1, Rotation3d()))
+    simsystem.AddCamera(simcam, Transform3d(0, 0, 1, Rotation3d()))
 
-simsystem.AddAprilTags(AprilTagFieldLayout.loadField(AprilTagField.k2024Crescendo))
+    simsystem.AddAprilTags(AprilTagFieldLayout.loadField(AprilTagField.k2024Crescendo))
 
-start = time.time()
-for i in range(10):
-    simsystem.Update(
-        Pose3d(Translation3d(8, 6, 0), Rotation3d(0, 0, time.time() - start))
-    )
-    time.sleep(0.02)
+    start = time.time()
+    for i in range(10):
+        simsystem.Update(
+            Pose3d(Translation3d(8, 6, 0), Rotation3d(0, 0, time.time() - start))
+        )
+        time.sleep(0.02)
