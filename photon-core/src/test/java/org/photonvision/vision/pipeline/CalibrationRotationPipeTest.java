1e389345a4b783f7efb4beaa280f2ce1d63426c0
Matt <matthew.morley.ca@gmail.com>
2024-10-16 22:40:53 -0700

    add rotated test

--- photon-core/src/test/java/org/photonvision/vision/pipeline/CalibrationRotationPipeTest.java
+++ photon-core/src/test/java/org/photonvision/vision/pipeline/CalibrationRotationPipeTest.java
@@ -3,7 +3,10 @@ package org.photonvision.vision.pipeline;
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 import java.io.IOException;
+
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 import org.junitpioneer.jupiter.cartesian.CartesianTest;
 import org.junitpioneer.jupiter.cartesian.CartesianTest.Enum;
 import org.opencv.calib3d.Calib3d;
@@ -15,9 +18,20 @@ import org.photonvision.common.logging.LogLevel;
 import org.photonvision.common.logging.Logger;
 import org.photonvision.common.util.TestUtils;
 import org.photonvision.mrcal.MrCalJNILoader;
+import org.photonvision.vision.apriltag.AprilTagFamily;
 import org.photonvision.vision.calibration.CameraCalibrationCoefficients;
+import org.photonvision.vision.camera.QuirkyCamera;
 import org.photonvision.vision.frame.FrameStaticProperties;
+import org.photonvision.vision.frame.provider.FileFrameProvider;
 import org.photonvision.vision.opencv.ImageRotationMode;
+import org.photonvision.vision.pipeline.result.CVPipelineResult;
+import org.photonvision.vision.target.TargetModel;
+
+import edu.wpi.first.math.geometry.Pose3d;
+import edu.wpi.first.math.geometry.Transform3d;
+import edu.wpi.first.math.geometry.Rotation3d;
+import edu.wpi.first.math.geometry.Translation3d;
+import edu.wpi.first.math.util.Units;
 
 public class CalibrationRotationPipeTest {
 
@@ -84,5 +98,41 @@ public class CalibrationRotationPipeTest {
         Point[] rotatedPoints = undistortedRotatedPoints.toArray();
 
         assertArrayEquals(expectedRotatedPoints, rotatedPoints);
+    }
+
+    @Test
+    public void testApriltagRotated() {
+        var pipeline = new AprilTagPipeline();
+
+        pipeline.getSettings().inputShouldShow = true;
+        pipeline.getSettings().outputShouldDraw = true;
+        pipeline.getSettings().solvePNPEnabled = true;
+        pipeline.getSettings().cornerDetectionAccuracyPercentage = 4;
+        pipeline.getSettings().cornerDetectionUseConvexHulls = true;
+        pipeline.getSettings().targetModel = TargetModel.kAprilTag6p5in_36h11;
+        pipeline.getSettings().tagFamily = AprilTagFamily.kTag16h5;
+
+        var frameProvider =
+                new FileFrameProvider(
+                        TestUtils.getApriltagImagePath(TestUtils.ApriltagTestImages.kTag_corner_1280, false),
+                        TestUtils.WPI2020Image.FOV,
+                        TestUtils.getCoeffs(TestUtils.LIMELIGHT_480P_CAL_FILE, false));
+        frameProvider.requestFrameThresholdType(pipeline.getThresholdType());
+
+        frameProvider.requestFrameRotation(ImageRotationMode.DEG_0);
+        CVPipelineResult pipelineResult = pipeline.run(frameProvider.get(), QuirkyCamera.DefaultCamera);
+        var pose_base = pipelineResult.targets.get(0).getBestCameraToTarget3d();
+
+        frameProvider.requestFrameRotation(ImageRotationMode.DEG_90_CCW);
+        CVPipelineResult pipelineResult2 = pipeline.run(frameProvider.get(), QuirkyCamera.DefaultCamera);
+        var pose_rotated = pipelineResult2.targets.get(0).getBestCameraToTarget3d();
+        var pose_unrotated = new Transform3d(new Translation3d(), new Rotation3d(Units.degreesToRadians(180), 0, 0)).plus(pose_rotated);
+
+        Assertions.assertEquals(pose_base.getX(), pose_unrotated.getX(), 0.01);
+        Assertions.assertEquals(pose_base.getY(), pose_unrotated.getY(), 0.01);
+        Assertions.assertEquals(pose_base.getZ(), pose_unrotated.getZ(), 0.01);
+        Assertions.assertEquals(pose_base.getRotation().getX(), pose_unrotated.getRotation().getX(), 0.01);
+        Assertions.assertEquals(pose_base.getRotation().getY(), pose_unrotated.getRotation().getY(), 0.01);
+        Assertions.assertEquals(pose_base.getRotation().getZ(), pose_unrotated.getRotation().getZ(), 0.01);
     }
 }
