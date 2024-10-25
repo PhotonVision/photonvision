6572d7327e22c8229e5bf921cd96599e49a079cd
Cameron (3539) <theforgelover@gmail.com>
2024-10-24 23:16:48 -0400

    Ope

--- photon-core/src/main/java/org/photonvision/common/configuration/ConfigManager.java
+++ photon-core/src/main/java/org/photonvision/common/configuration/ConfigManager.java
@@ -258,7 +258,7 @@ public class ConfigManager {
     }
 
     public Path getCalibrationImageSavePath(String uniqueCameraName) {
-        var imgFilePath = Path.of(configDirectoryFile.toString(), "calibration").toFile();
+        var imgFilePath = Path.of(configDirectoryFile.toString(), "calibration", uniqueCameraName).toFile();
         if (!imgFilePath.exists()) imgFilePath.mkdirs();
         return imgFilePath.toPath();
     }
