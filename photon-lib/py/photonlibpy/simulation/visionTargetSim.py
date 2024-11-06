61c0c3337e71f45238fb15e2d7d703ab2a785cb9
Lucien Morey <lucienp@live.com.au>
2024-11-06 19:16:58 +1100

    fix capitalisation on geometry methods

--- photon-lib/py/photonlibpy/simulation/visionTargetSim.py
+++ photon-lib/py/photonlibpy/simulation/visionTargetSim.py
@@ -14,22 +14,22 @@ class VisionTargetSim:
         self.objDetConf: float = -1.0
 
     def __lt__(self, right) -> bool:
-        return self.pose.Translation().Norm() < right.pose.Translation().Norm()
+        return self.pose.translation().norm() < right.pose.translation().norm()
 
     def __eq__(self, other) -> bool:
         # Use 1 inch and 1 degree tolerance
         return (
-            abs(self.pose.Translation().X() - other.GetPose().Translation().X())
+            abs(self.pose.translation().X() - other.GetPose().translation().X())
             < 0.0254
-            and abs(self.pose.Translation().Y() - other.GetPose().Translation().Y())
+            and abs(self.pose.translation().Y() - other.GetPose().translation().Y())
             < 0.0254
-            and abs(self.pose.Translation().Z() - other.GetPose().Translation().Z())
+            and abs(self.pose.translation().Z() - other.GetPose().translation().Z())
             < 0.0254
-            and abs(self.pose.Rotation().X() - other.GetPose().Rotation().X())
+            and abs(self.pose.rotation().X() - other.GetPose().rotation().X())
             < math.radians(1)
-            and abs(self.pose.Rotation().Y() - other.GetPose().Rotation().Y())
+            and abs(self.pose.rotation().Y() - other.GetPose().rotation().Y())
             < math.radians(1)
-            and abs(self.pose.Rotation().Z() - other.GetPose().Rotation().Z())
+            and abs(self.pose.rotation().Z() - other.GetPose().rotation().Z())
             < math.radians(1)
             and self.model.GetIsPlanar() == other.GetModel().GetIsPlanar()
         )
