75d6b1c831f59d5f44116c7306ca6ce468e53158
Lucien Morey <lucienp@live.com.au>
2024-11-06 23:35:19 +1100

    work on constuctor and submitProcessedFrame

--- photon-lib/py/photonlibpy/simulation/photonCameraSim.py
+++ photon-lib/py/photonlibpy/simulation/photonCameraSim.py
@@ -3,9 +3,12 @@ from .visionTargetSim import VisionTargetSim
 from ..photonCamera import PhotonCamera
 from ..targeting import PhotonPipelineResult
 from ..estimation.cameraTargetRelation import CameraTargetRelation
+from ..networktables.NTTopicSet import NTTopicSet
+from ..packet import Packet
 
-from wpimath.geometry import Pose3d
+from wpimath.geometry import Pose3d, Transform3d
 from wpimath.units import meters, seconds
+import wpilib
 
 import cscore as cs
 import numpy as np
@@ -14,6 +17,8 @@ import typing
 
 
 class PhotonCameraSim:
+    kDefaultMinAreaPx: float = 100
+
     def __init__(
         self,
         camera: PhotonCamera,
@@ -21,25 +26,82 @@ class PhotonCameraSim:
         minTargetAreaPercent: float | None = None,
         maxSightRange: meters | None = None,
     ):
-        self.videoSimRaw: cs.CvSource
-        self.videoSimFrameRaw: np.ndarray
-        self.videoSimRawEnabled: bool = True
-        self.videoSimWireframeEnabled: bool = False
-        self.videoSimWireframeResolution: float = 0.1
-        self.videoSimProcessed: cs.CvSource
-        self.videoSimFrameProcessed: np.ndarray
-        self.videoSimProcEnabled: bool = True
-
-        self.cam: PhotonCamera = camera
-        self.prop = props if props else PERFECT_90DEG()
 
         self.minTargetAreaPercent: float = 0.0
         self.maxSightRange: float = 1.0e99
+        self.videoSimRawEnabled: bool = True
+        self.videoSimWireframeEnabled: bool = False
+        self.videoSimWireframeResolution: float = 0.1
+        self.videoSimProcEnabled: bool = True
+        self.ts = NTTopicSet()
+        self.heartbeatCounter: int = 0
+        self.nextNtEntryTime = int(wpilib.Timer.getFPGATimestamp() * 1e6)
 
-        if minTargetAreaPercent is not None and maxSightRange is not None:
+        if (
+            camera is not None
+            and props is None
+            and minTargetAreaPercent is None
+            and maxSightRange is None
+        ):
+            props = PERFECT_90DEG()
+        elif (
+            camera is not None
+            and props is not None
+            and minTargetAreaPercent is not None
+            and maxSightRange is not None
+        ):
+            pass
+        elif (
+            camera is not None
+            and props is not None
+            and minTargetAreaPercent is None
+            and maxSightRange is None
+        ):
+            pass
+        else:
+            raise Exception("Invalid Constructor Called")
+
+        self.cam = camera
+        self.prop = props
+        self.setMinTargetAreaPixels(PhotonCameraSim.kDefaultMinAreaPx)
+
+        # TODO Check fps is right
+        self.videoSimRaw = cs.CvSource(
+            self.cam.getName() + "-raw",
+            cs.VideoMode.PixelFormat.kGray,
+            self.prop.getResWidth(),
+            self.prop.getResHeight(),
+            1,
+        )
+        self.videoSimFrameRaw = np.zeros(
+            (self.prop.getResWidth(), self.prop.getResHeight())
+        )
+
+        # TODO Check fps is right
+        self.videoSimProcessed = cs.CvSource(
+            self.cam.getName() + "-processed",
+            cs.VideoMode.PixelFormat.kGray,
+            self.prop.getResWidth(),
+            self.prop.getResHeight(),
+            1,
+        )
+        self.videoSimFrameProcessed = np.zeros(
+            (self.prop.getResWidth(), self.prop.getResHeight())
+        )
+
+        # TODO what is the syntax to get and set the right nt subtable here
+        # self.ts.subTable = self.cam.
+        self.ts.updateEntries()
+
+        # Handle this last explicitly for this function signature because the other constructor is called in the initialiser list
+        if (
+            camera is not None
+            and props is not None
+            and minTargetAreaPercent is not None
+            and maxSightRange is not None
+        ):
             self.minTargetAreaPercent = minTargetAreaPercent
             self.maxSightRange = maxSightRange
-        # TODO Initialise video stream stuff
 
     def getCamera(self) -> PhotonCamera:
         return self.cam
@@ -129,4 +191,46 @@ class PhotonCameraSim:
     def submitProcessedFrame(
         self, result: PhotonPipelineResult, receiveTimestamp: float | None
     ):
-        raise Exception("Not yet implemented")
+        if receiveTimestamp is None:
+            receiveTimestamp = wpilib.Timer.getFPGATimestamp() * 1e6
+        receiveTimestamp = int(receiveTimestamp)
+
+        self.ts.latencyMillisEntry.set(result.getLatencyMillis(), receiveTimestamp)
+
+        #### TODO Create and send out Packet
+        # newPacket = Packet()
+        # newPacket.Pack(result)
+        #
+        # self.ts.rawBytesEntry.set(newPacket.getData(), receiveTimestamp)
+
+        hasTargets = result.hasTargets()
+        self.ts.hasTargetEntry.set(hasTargets, receiveTimestamp)
+        if not hasTargets:
+            self.ts.targetPitchEntry.set(0.0, receiveTimestamp)
+            self.ts.targetYawEntry.set(0.0, receiveTimestamp)
+            self.ts.targetAreaEntry.set(0.0, receiveTimestamp)
+            self.ts.targetPoseEntry.set(Transform3d(), receiveTimestamp)
+            self.ts.targetSkewEntry.set(0.0, receiveTimestamp)
+        else:
+            bestTarget = result.getBestTarget()
+
+            self.ts.targetPitchEntry.set(bestTarget.getPitch(), receiveTimestamp)
+            self.ts.targetYawEntry.set(bestTarget.getYaw(), receiveTimestamp)
+            self.ts.targetAreaEntry.set(bestTarget.getArea(), receiveTimestamp)
+            self.ts.targetSkewEntry.set(bestTarget.getSkew(), receiveTimestamp)
+
+            self.ts.targetPoseEntry.set(
+                bestTarget.getBestCameraToTarget(), receiveTimestamp
+            )
+
+            intrinsics = self.prop.getIntrinsics()
+            intrinsicsView = intrinsics.tolist()
+            self.ts.cameraIntrinsicsPublisher.set(intrinsicsView, receiveTimestamp)
+
+            distortion = self.prop.getDistCoeffs()
+            distortionView = distortion.tolist()
+            self.ts.cameraDistortionPublisher.set(distortionView, receiveTimestamp)
+
+            self.ts.heartbeatPublisher.set(self.heartbeatCounter, receiveTimestamp)
+
+            self.ts.subTable.getInstance().flush()
