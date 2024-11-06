24eb29da1cfbd964eb73c0688486681d983c2f81
James Ward <james@robomo.co>
2024-11-06 15:53:11 +1100

    Add generating functions for AprilTag target types

--- photon-lib/py/photonlibpy/simulation/targetModel.py
+++ photon-lib/py/photonlibpy/simulation/targetModel.py
@@ -1,2 +1,11 @@
 class TargetModel:
-    pass
+    def __init__(self, width: float, height: float):
+        pass
+
+
+def AprilTag36h11():
+    return TargetModel(6.5 * 0.0254, 6.5 * 0.0254)
+
+
+def AprilTag16h5():
+    return TargetModel(6.0 * 0.0254, 6.0 * 0.0254)
