1657ee36976bc2fc434ba22976d831268f0fbb60
James Ward <james@robomo.co>
2024-11-06 13:42:12 +1100

    Add blank TargetModel class

--- photon-lib/py/photonlibpy/simulation/__init__.py
+++ photon-lib/py/photonlibpy/simulation/__init__.py
@@ -1,5 +1,6 @@
 from .photonCameraSim import PhotonCameraSim
 from .simCameraProperties import SimCameraProperties
+from .targetModel import TargetModel
 from .videoSimUtil import VideoSimUtil
 from .visionSystemSim import VisionSystemSim
 from .visionTargetSim import VisionTargetSim
