b73ffc1610b8ec62d6ba7d8a8a001e46cff1a8c6
Lucien Morey <lucienp@live.com.au>
2024-11-06 21:12:57 +1100

    remove rogue import

--- photon-lib/py/photonlibpy/simulation/__init__.py
+++ photon-lib/py/photonlibpy/simulation/__init__.py
@@ -1,6 +1,5 @@
 from .photonCameraSim import PhotonCameraSim
 from .simCameraProperties import SimCameraProperties
-from .targetModel import TargetModel
 from .videoSimUtil import VideoSimUtil
 from .visionSystemSim import VisionSystemSim
 from .visionTargetSim import VisionTargetSim
