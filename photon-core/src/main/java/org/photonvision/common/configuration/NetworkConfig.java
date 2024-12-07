PhotonVision/photonvision

commit adb18fe7119facd888b72d69aeb6941db60de482

Refactor program configuration broadcast hashmap spaghetti (#1592)
WAS: we used raw hash-maps to encode program state
S/B: we use Jackson to do this encoding for us for free. We have
Objects, and we should use them to represent structured data.

---------

Co-authored-by: Craig Schardt <crschardt@fastem.com>

photon-core/src/main/java/org/photonvision/common/configuration/NetworkConfig.java
@@ -89,6 +89,21 @@ public class NetworkConfig {
         setShouldManage(shouldManage);
     }
 
+    public NetworkConfig(NetworkConfig config) {
+        this(
+                config.ntServerAddress,
+                config.connectionType,
+                config.staticIp,
+                config.hostname,
+                config.runNTServer,
+                config.shouldManage,
+                config.shouldPublishProto,
+                config.networkManagerIface,
+                config.setStaticCommand,
+                config.setDHCPcommand,
+                config.matchCamerasOnlyByPath);
+    }
+
     public Map<String, Object> toHashMap() {
         try {
             var ret = new ObjectMapper().convertValue(this, JacksonUtils.UIMap.class);

photon-core/src/main/java/org/photonvision/common/dataflow/websocket/UINetConfig.java
@@ -1,0 +1,34 @@
+/*
+ * Copyright (C) Photon Vision.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package org.photonvision.common.dataflow.websocket;
+
+import java.util.List;
+import org.photonvision.common.configuration.NetworkConfig;
+import org.photonvision.common.networking.NetworkUtils.NMDeviceInfo;
+
+public class UINetConfig extends NetworkConfig {
+    public UINetConfig(
+            NetworkConfig config, List<NMDeviceInfo> networkInterfaceNames, boolean networkingDisabled) {
+        super(config);
+        this.networkInterfaceNames = networkInterfaceNames;
+        this.networkingDisabled = networkingDisabled;
+    }
+
+    public List<NMDeviceInfo> networkInterfaceNames;
+    public boolean networkingDisabled;
+}

photon-core/src/main/java/org/photonvision/common/configuration/PhotonConfiguration.java
@@ -21,19 +21,6 @@ import edu.wpi.first.apriltag.AprilTagFieldLayout;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
-import org.photonvision.PhotonVersion;
-import org.photonvision.common.hardware.Platform;
-import org.photonvision.common.networking.NetworkManager;
-import org.photonvision.common.networking.NetworkUtils;
-import org.photonvision.common.util.SerializationUtils;
-import org.photonvision.mrcal.MrCalJNILoader;
-import org.photonvision.raspi.LibCameraJNILoader;
-import org.photonvision.vision.calibration.UICameraCalibrationCoefficients;
-import org.photonvision.vision.camera.QuirkyCamera;
-import org.photonvision.vision.processes.VisionModule;
-import org.photonvision.vision.processes.VisionModuleManager;
 import org.photonvision.vision.processes.VisionSource;
 
 public class PhotonConfiguration {
@@ -122,81 +109,6 @@ public class PhotonConfiguration {
      */
     public boolean removeCameraConfig(String name) {
         return cameraConfigurations.remove(name) != null;
-    }
-
-    public Map<String, Object> toHashMap() {
-        Map<String, Object> map = new HashMap<>();
-        var settingsSubmap = new HashMap<String, Object>();
-
-        // Hack active interfaces into networkSettings
-        var netConfigMap = networkConfig.toHashMap();
-        netConfigMap.put("networkInterfaceNames", NetworkUtils.getAllActiveWiredInterfaces());
-        netConfigMap.put("networkingDisabled", NetworkManager.getInstance().networkingIsDisabled);
-
-        settingsSubmap.put("networkSettings", netConfigMap);
-
-        var lightingConfig = new UILightingConfig();
-        lightingConfig.brightness = hardwareSettings.ledBrightnessPercentage;
-        lightingConfig.supported = !hardwareConfig.ledPins.isEmpty();
-        settingsSubmap.put("lighting", SerializationUtils.objectToHashMap(lightingConfig));
-        // General Settings
-        var generalSubmap = new HashMap<String, Object>();
-        generalSubmap.put("version", PhotonVersion.versionString);
-        generalSubmap.put(
-                "gpuAcceleration",
-                LibCameraJNILoader.isSupported()
-                        ? "Zerocopy Libcamera Working"
-                        : ""); // TODO add support for other types of GPU accel
-        generalSubmap.put("mrCalWorking", MrCalJNILoader.getInstance().isLoaded());
-        generalSubmap.put("availableModels", NeuralNetworkModelManager.getInstance().getModels());
-        generalSubmap.put(
-                "supportedBackends", NeuralNetworkModelManager.getInstance().getSupportedBackends());
-        generalSubmap.put(
-                "hardwareModel",
-                hardwareConfig.deviceName.isEmpty()
-                        ? Platform.getHardwareModel()
-                        : hardwareConfig.deviceName);
-        generalSubmap.put("hardwarePlatform", Platform.getPlatformName());
-        settingsSubmap.put("general", generalSubmap);
-        // AprilTagFieldLayout
-        settingsSubmap.put("atfl", this.atfl);
-
-        map.put(
-                "cameraSettings",
-                VisionModuleManager.getInstance().getModules().stream()
-                        .map(VisionModule::toUICameraConfig)
-                        .map(SerializationUtils::objectToHashMap)
-                        .collect(Collectors.toList()));
-        map.put("settings", settingsSubmap);
-
-        return map;
-    }
-
-    public static class UILightingConfig {
-        public int brightness = 0;
-        public boolean supported = true;
-    }
-
-    public static class UICameraConfiguration {
-        @SuppressWarnings("unused")
-        public double fov;
-
-        public String nickname;
-        public String uniqueName;
-        public HashMap<String, Object> currentPipelineSettings;
-        public int currentPipelineIndex;
-        public List<String> pipelineNicknames;
-        public HashMap<Integer, HashMap<String, Object>> videoFormatList;
-        public int outputStreamPort;
-        public int inputStreamPort;
-        public List<UICameraCalibrationCoefficients> calibrations;
-        public boolean isFovConfigurable = true;
-        public QuirkyCamera cameraQuirks;
-        public boolean isCSICamera;
-        public double minExposureRaw;
-        public double maxExposureRaw;
-        public double minWhiteBalanceTemp;
-        public double maxWhiteBalanceTemp;
     }
 
     @Override

photon-core/src/main/java/org/photonvision/common/dataflow/networktables/NetworkTablesManager.java
@@ -32,6 +32,7 @@ import org.photonvision.common.configuration.ConfigManager;
 import org.photonvision.common.configuration.NetworkConfig;
 import org.photonvision.common.dataflow.DataChangeService;
 import org.photonvision.common.dataflow.events.OutgoingUIEvent;
+import org.photonvision.common.dataflow.websocket.UIPhotonConfiguration;
 import org.photonvision.common.hardware.HardwareManager;
 import org.photonvision.common.logging.LogGroup;
 import org.photonvision.common.logging.LogLevel;
@@ -165,7 +166,8 @@ public class NetworkTablesManager {
             DataChangeService.getInstance()
                     .publishEvent(
                             new OutgoingUIEvent<>(
-                                    "fullsettings", ConfigManager.getInstance().getConfig().toHashMap()));
+                                    "fullsettings",
+                                    UIPhotonConfiguration.programStateToUi(ConfigManager.getInstance().getConfig())));
         } catch (IOException e) {
             logger.error("Error deserializing atfl!");
             logger.error(atfl_json);

photon-core/src/main/java/org/photonvision/common/dataflow/websocket/UICameraConfiguration.java
@@ -1,0 +1,45 @@
+/*
+ * Copyright (C) Photon Vision.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package org.photonvision.common.dataflow.websocket;
+
+import java.util.HashMap;
+import java.util.List;
+import org.photonvision.vision.calibration.UICameraCalibrationCoefficients;
+import org.photonvision.vision.camera.QuirkyCamera;
+
+public class UICameraConfiguration {
+    @SuppressWarnings("unused")
+    public double fov;
+
+    public String nickname;
+    public String uniqueName;
+    public HashMap<String, Object> currentPipelineSettings;
+    public int currentPipelineIndex;
+    public List<String> pipelineNicknames;
+    public HashMap<Integer, HashMap<String, Object>> videoFormatList;
+    public int outputStreamPort;
+    public int inputStreamPort;
+    public List<UICameraCalibrationCoefficients> calibrations;
+    public boolean isFovConfigurable = true;
+    public QuirkyCamera cameraQuirks;
+    public boolean isCSICamera;
+    public double minExposureRaw;
+    public double maxExposureRaw;
+    public double minWhiteBalanceTemp;
+    public double maxWhiteBalanceTemp;
+}

photon-core/src/main/java/org/photonvision/common/dataflow/websocket/UIGeneralSettings.java
@@ -1,0 +1,49 @@
+/*
+ * Copyright (C) Photon Vision.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package org.photonvision.common.dataflow.websocket;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class UIGeneralSettings {
+    public UIGeneralSettings(
+            String version,
+            String gpuAcceleration,
+            boolean mrCalWorking,
+            Map<String, ArrayList<String>> availableModels,
+            List<String> supportedBackends,
+            String hardwareModel,
+            String hardwarePlatform) {
+        this.version = version;
+        this.gpuAcceleration = gpuAcceleration;
+        this.mrCalWorking = mrCalWorking;
+        this.availableModels = availableModels;
+        this.supportedBackends = supportedBackends;
+        this.hardwareModel = hardwareModel;
+        this.hardwarePlatform = hardwarePlatform;
+    }
+
+    public String version;
+    public String gpuAcceleration;
+    public boolean mrCalWorking;
+    public Map<String, ArrayList<String>> availableModels;
+    public List<String> supportedBackends;
+    public String hardwareModel;
+    public String hardwarePlatform;
+}

photon-core/src/main/java/org/photonvision/common/dataflow/websocket/UILightingConfig.java
@@ -1,0 +1,28 @@
+/*
+ * Copyright (C) Photon Vision.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package org.photonvision.common.dataflow.websocket;
+
+public class UILightingConfig {
+    public UILightingConfig(int brightness, boolean supported) {
+        this.brightness = brightness;
+        this.supported = supported;
+    }
+
+    public int brightness = 0;
+    public boolean supported = true;
+}

photon-core/src/main/java/org/photonvision/common/dataflow/websocket/UIPhotonConfiguration.java
@@ -1,0 +1,69 @@
+/*
+ * Copyright (C) Photon Vision.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package org.photonvision.common.dataflow.websocket;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import org.photonvision.PhotonVersion;
+import org.photonvision.common.configuration.NeuralNetworkModelManager;
+import org.photonvision.common.configuration.PhotonConfiguration;
+import org.photonvision.common.hardware.Platform;
+import org.photonvision.common.networking.NetworkManager;
+import org.photonvision.common.networking.NetworkUtils;
+import org.photonvision.mrcal.MrCalJNILoader;
+import org.photonvision.raspi.LibCameraJNILoader;
+import org.photonvision.vision.processes.VisionModule;
+import org.photonvision.vision.processes.VisionModuleManager;
+
+public class UIPhotonConfiguration {
+    public List<UICameraConfiguration> cameraSettings;
+    public UIProgramSettings settings;
+
+    public UIPhotonConfiguration(
+            UIProgramSettings settings, List<UICameraConfiguration> cameraSettings) {
+        this.cameraSettings = cameraSettings;
+        this.settings = settings;
+    }
+
+    public static UIPhotonConfiguration programStateToUi(PhotonConfiguration c) {
+        return new UIPhotonConfiguration(
+                new UIProgramSettings(
+                        new UINetConfig(
+                                c.getNetworkConfig(),
+                                NetworkUtils.getAllActiveWiredInterfaces(),
+                                NetworkManager.getInstance().networkingIsDisabled),
+                        new UILightingConfig(
+                                c.getHardwareSettings().ledBrightnessPercentage,
+                                !c.getHardwareConfig().ledPins.isEmpty()),
+                        new UIGeneralSettings(
+                                PhotonVersion.versionString,
+                                // TODO add support for other types of GPU accel
+                                LibCameraJNILoader.isSupported() ? "Zerocopy Libcamera Working" : "",
+                                MrCalJNILoader.getInstance().isLoaded(),
+                                NeuralNetworkModelManager.getInstance().getModels(),
+                                NeuralNetworkModelManager.getInstance().getSupportedBackends(),
+                                c.getHardwareConfig().deviceName.isEmpty()
+                                        ? Platform.getHardwareModel()
+                                        : c.getHardwareConfig().deviceName,
+                                Platform.getPlatformName()),
+                        c.getApriltagFieldLayout()),
+                VisionModuleManager.getInstance().getModules().stream()
+                        .map(VisionModule::toUICameraConfig)
+                        .collect(Collectors.toList()));
+    }
+}

commit 782929b006ebe46e5cde6d737ab9d354dae4374e

Fix "Manage Device Networking" toggle being disabled incorrectly (#1620)
This fixes a bug introduced in #1592 that caused the Manage Device
Networking toggle to be disabled for systems where PhotonVision is
managing the network.

There is still a problem with the toggle defaulting to "off" and not
staying in the "on" position after settings are saved. I need help from
someone who understands the frontend to figure out why it keeps getting
set back to "off".

---------

Co-authored-by: Matt <matthew.morley.ca@gmail.com>
Co-authored-by: Cameron (3539) <theforgelover@gmail.com>

photon-core/src/main/java/org/photonvision/common/configuration/NetworkConfig.java
@@ -21,12 +21,8 @@ import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import java.util.HashMap;
-import java.util.Map;
 import org.photonvision.common.hardware.Platform;
 import org.photonvision.common.networking.NetworkMode;
-import org.photonvision.common.util.file.JacksonUtils;
 
 public class NetworkConfig {
     // Can be an integer team number, or an IP address
@@ -104,17 +100,6 @@ public class NetworkConfig {
                 config.matchCamerasOnlyByPath);
     }
 
-    public Map<String, Object> toHashMap() {
-        try {
-            var ret = new ObjectMapper().convertValue(this, JacksonUtils.UIMap.class);
-            ret.put("canManage", this.deviceCanManageNetwork());
-            return ret;
-        } catch (Exception e) {
-            e.printStackTrace();
-            return new HashMap<>();
-        }
-    }
-
     @JsonIgnore
     public String getPhysicalInterfaceName() {
         return this.networkManagerIface;
@@ -125,18 +110,12 @@ public class NetworkConfig {
         return "\"" + networkManagerIface + "\"";
     }
 
-    @JsonIgnore
-    public boolean shouldManage() {
-        return this.shouldManage;
-    }
-
-    @JsonIgnore
     public void setShouldManage(boolean shouldManage) {
         this.shouldManage = shouldManage && this.deviceCanManageNetwork();
     }
 
     @JsonIgnore
-    private boolean deviceCanManageNetwork() {
+    protected boolean deviceCanManageNetwork() {
         return Platform.isLinux();
     }
 

photon-core/src/main/java/org/photonvision/common/dataflow/websocket/UINetConfig.java
@@ -27,8 +27,10 @@ public class UINetConfig extends NetworkConfig {
         super(config);
         this.networkInterfaceNames = networkInterfaceNames;
         this.networkingDisabled = networkingDisabled;
+        this.canManage = this.deviceCanManageNetwork();
     }
 
     public List<NMDeviceInfo> networkInterfaceNames;
     public boolean networkingDisabled;
+    public boolean canManage;
 }

photon-core/src/main/java/org/photonvision/common/networking/NetworkManager.java
@@ -113,7 +113,7 @@ public class NetworkManager {
     }
 
     public void reinitialize() {
-        initialize(ConfigManager.getInstance().getConfig().getNetworkConfig().shouldManage());
+        initialize(ConfigManager.getInstance().getConfig().getNetworkConfig().shouldManage);
 
         DataChangeService.getInstance()
                 .publishEvent(