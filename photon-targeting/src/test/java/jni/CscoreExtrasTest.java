e08b07c4fadc16144176b171bbf85371315750be
Matt <matthew.morley.ca@gmail.com>
2024-12-28 12:16:36 -0800

    Create cscoreextras

---
+++ photon-targeting/src/test/java/jni/CscoreExtrasTest.java
@@ -0,0 +1,108 @@
+/*
+ * Copyright (C) Photon Vision.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package jni;
+
+import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assumptions.assumeTrue;
+
+import edu.wpi.first.cameraserver.CameraServer;
+import edu.wpi.first.cscore.CameraServerJNI;
+import edu.wpi.first.cscore.CvSink;
+import edu.wpi.first.cscore.CvSource;
+import edu.wpi.first.cscore.UsbCamera;
+import edu.wpi.first.hal.HAL;
+import edu.wpi.first.util.PixelFormat;
+import edu.wpi.first.util.RawFrame;
+import java.io.IOException;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.opencv.core.Mat;
+import org.opencv.imgcodecs.Imgcodecs;
+import org.photonvision.jni.CscoreExtras;
+import org.photonvision.jni.PhotonTargetingJniLoader;
+import org.photonvision.jni.WpilibLoader;
+
+public class CscoreExtrasTest {
+    @BeforeAll
+    public static void load_wpilib() throws UnsatisfiedLinkError, IOException {
+        if (!WpilibLoader.loadLibraries()) {
+            fail();
+        }
+        if (!PhotonTargetingJniLoader.load()) {
+            fail();
+        }
+
+        HAL.initialize(1000, 0);
+    }
+
+    @AfterAll
+    public static void teardown() {
+        HAL.shutdown();
+    }
+
+    @Test
+    public void testCaptureImage() {
+        assumeTrue(CameraServerJNI.enumerateUsbCameras().length > 0);
+
+        UsbCamera camera = CameraServer.startAutomaticCapture(2);
+
+        camera.setResolution(640, 480);
+        var cameraMode = camera.getVideoMode();
+
+        CvSink cvSink = CameraServer.getVideo(camera);
+
+        CvSource outputStream = CameraServer.putVideo("Detected", 640, 480);
+
+        long lastTime = 0;
+        for (int i = 0; i < 10000; i++) {
+            var frame = new RawFrame();
+            frame.setInfo(
+                    cameraMode.width,
+                    cameraMode.height,
+                    // hard-coded 3 channel
+                    cameraMode.width * 3,
+                    PixelFormat.kBGR);
+            long time =
+                    CscoreExtras.grabRawSinkFrameTimeoutLastTime(
+                            cvSink.getHandle(), frame.getNativeObj(), 0.225, lastTime);
+
+            if (time != 0) {
+                var mat = new Mat(CscoreExtras.wrapRawFrame(frame.getNativeObj()));
+
+                System.out.println(mat);
+                System.out.println(
+                        "Mat is " + mat.cols() + " x " + mat.rows() + " (cols x rows) with type " + mat.type());
+
+                Imgcodecs.imwrite("out.png", mat);
+                outputStream.putFrame(mat);
+
+                mat.release();
+            } else {
+                System.err.println("cannot wrap a null mat");
+            }
+
+            lastTime = time;
+
+            frame.close();
+        }
+
+        cvSink.close();
+        camera.close();
+    }
+}
