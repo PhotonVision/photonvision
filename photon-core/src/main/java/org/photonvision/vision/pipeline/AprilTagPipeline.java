0362220f293ecc4c8e633f5cc49a6b76cd695de9
GGreenix <76048260+GGreenix@users.noreply.github.com>
2024-12-30 21:35:07 +0200

    revert commit

--- photon-core/src/main/java/org/photonvision/vision/pipeline/AprilTagPipeline.java
+++ photon-core/src/main/java/org/photonvision/vision/pipeline/AprilTagPipeline.java
@@ -152,12 +152,12 @@ public class AprilTagPipeline extends CVPipeline<CVPipelineResult, AprilTagPipel
         sumPipeNanosElapsed += croppedFrame.nanosElapsed;
 
         CVPipeResult<List<AprilTagDetection>> tagDetectionPipeResult;
-        tagDetectionPipeResult = aprilTagDetectionPipe.run(frame.processedImage);
+        tagDetectionPipeResult = aprilTagDetectionPipe.run(croppedFrame.output);
         sumPipeNanosElapsed += tagDetectionPipeResult.nanosElapsed;
         croppedFrame.output.release();
 
-        // tagDetectionPipeResult = uncropPipe.run(tagDetectionPipeResult.output);
-        // sumPipeNanosElapsed += tagDetectionPipeResult.nanosElapsed;
+        tagDetectionPipeResult = uncropPipe.run(tagDetectionPipeResult.output);
+        sumPipeNanosElapsed += tagDetectionPipeResult.nanosElapsed;
 
         List<AprilTagDetection> detections = tagDetectionPipeResult.output;
         List<AprilTagDetection> usedDetections = new ArrayList<>();
