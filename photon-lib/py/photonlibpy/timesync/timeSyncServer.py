435a74be6b478a2c43432b19d592a4cedfa56543
Chris <chrisgerth010592@gmail.com>
2025-01-02 14:16:52 -0600

    format and struct

--- photon-lib/py/photonlibpy/timesync/timeSyncServer.py
+++ photon-lib/py/photonlibpy/timesync/timeSyncServer.py
@@ -1,43 +1,56 @@
 import threading
 import socket
-from typing import Callable, Optional
-from wpilib import Timer
+import struct
 import logging
+from typing import Callable, Optional
+
+from wpilib import Timer
+
+
+class TspPing:
+    def __init__(self, version: int, message_id: int, client_time: int):
+        self.version = version
+        self.message_id = message_id
+        self.client_time = client_time
+
+    @staticmethod
+    def unpack(data: bytes) -> "TspPing":
+        # Unpack using struct.unpack
+        version, message_id, client_time = struct.unpack("<BBQ", data)
+        return TspPing(version, message_id, client_time)
+
+    def pack(self) -> bytes:
+        # Pack using struct.pack
+        return struct.pack("<BBQ", self.version, self.message_id, self.client_time)
+
+
+class TspPong:
+    def __init__(self, ping: "TspPing", server_time: int):
+        self.version = ping.version
+        self.message_id = 2  # Pong message ID
+        self.client_time = ping.client_time
+        self.server_time = server_time
+
+    def pack(self) -> bytes:
+        # Pack using struct.pack
+        return struct.pack(
+            "<BBQQ", self.version, self.message_id, self.client_time, self.server_time
+        )
+
+    @staticmethod
+    def unpack(data: bytes) -> "TspPong":
+        # Unpack using struct.unpack
+        version, message_id, client_time, server_time = struct.unpack("<BBQQ", data)
+        ping = TspPing(version, message_id, client_time)
+        return TspPong(ping, server_time)
 
 
 class TimeSyncServer:
-    """This class is a python re-write of the UDP time sync server protocol 
+    """This class is a python re-write of the UDP time sync server protocol
     which runs on a roboRIO to establish a timebase for all PhotonVision coprocessors.
     """
+
     PORT = 5810
-
-    class TspPing:
-        def __init__(self, version: int, message_id: int, client_time: int):
-            self.version = version
-            self.message_id = message_id
-            self.client_time = client_time
-
-        @staticmethod
-        def unpack(data: bytes) -> 'TimeSyncServer.TspPing':
-            version = data[0]
-            message_id = data[1]
-            client_time = int.from_bytes(data[2:10], byteorder='little')
-            return TimeSyncServer.TspPing(version, message_id, client_time)
-
-    class TspPong:
-        def __init__(self, ping: 'TimeSyncServer.TspPing', server_time: int):
-            self.version = ping.version
-            self.message_id = 2  # Pong message ID
-            self.client_time = ping.client_time
-            self.server_time = server_time
-
-        def pack(self) -> bytes:
-            return (
-                self.version.to_bytes(1, byteorder='little') +
-                self.message_id.to_bytes(1, byteorder='little') +
-                self.client_time.to_bytes(8, byteorder='little') +
-                self.server_time.to_bytes(8, byteorder='little')
-            )
 
     def __init__(self, time_provider: Optional[Callable[[], int]] = None):
         self.time_provider = time_provider or Timer.getFPGATimestamp
@@ -49,26 +62,29 @@ class TimeSyncServer:
             udp_socket.bind(("0.0.0.0", self.PORT))
             while True:
                 data, addr = udp_socket.recvfrom(1024)  # Buffer size of 1024 bytes
-                self.logger.debug( f"Data: { ''.join([format(x, '02x') for x in data]) }" )
+                self.logger.debug(
+                    f"Data: { ''.join([format(x, '02x') for x in data]) }"
+                )
 
                 if len(data) < 10:
                     self.logger.error("Too few bytes")
                     continue  # Ignore incomplete packets
 
-                ping = self.TspPing.unpack(data)
+                ping = TspPing.unpack(data)
                 if ping.version != 1 or ping.message_id != 1:
                     self.logger.error("Invalid Version/ID")
                     continue  # Ignore invalid pings
 
                 server_time = int(self.time_provider() * 1e6)  # Convert to microseconds
-                pong = self.TspPong(ping, server_time)
+                pong = TspPong(ping, server_time)
                 udp_socket.sendto(pong.pack(), addr)
-                self.logger.debug(f"Ponged at {server_time} with { ''.join([format(x, '02x') for x in pong.pack()]) }")
-
+                self.logger.debug(
+                    f"Ponged at {server_time} with { ''.join([format(x, '02x') for x in pong.pack()]) }"
+                )
 
     def start(self):
         if self._process is not None and self._process.is_alive():
-            return # Nothing to do
+            return  # Nothing to do
 
         self._process = threading.Thread(target=self._udp_server, daemon=True)
         self._process.start()
@@ -79,5 +95,6 @@ class TimeSyncServer:
             self._process.join()
             self._process = None
             self.logger.info("Server Stopped")
+
 
 inst = TimeSyncServer()
