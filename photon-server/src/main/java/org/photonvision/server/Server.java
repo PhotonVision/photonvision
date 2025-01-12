c4d67fb9d7ec67c719a9f63485d1bd68428fff14
Sam Freund <techguy763@gmail.com>
2025-01-12 00:09:26 -0600

    Update Server.java
    forgot to clean that up

--- photon-server/src/main/java/org/photonvision/server/Server.java
+++ photon-server/src/main/java/org/photonvision/server/Server.java
@@ -130,12 +130,6 @@ public class Server {
         app.post(
                 "/api/utils/importObjectDetectionModel",
                 RequestHandler::onImportObjectDetectionModelRequest);
-        app.post(
-                "api/utils/deleteObjectDetectionModel",
-                RequestHandler::onDeleteObjectDetectionModelRequest);
-        app.post(
-                "api/utils/editObjectDetectionModelName",
-                RequestHandler::onEditObjectDetectionModelNameRequest);
         app.get("/api/utils/photonvision-journalctl.txt", RequestHandler::onLogExportRequest);
         app.post("/api/utils/restartProgram", RequestHandler::onProgramRestartRequest);
         app.post("/api/utils/restartDevice", RequestHandler::onDeviceRestartRequest);
