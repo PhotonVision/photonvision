d1cbd2d9ed2afa348329ab2c227517e897a5ff15
raylmorris <=>
2025-01-24 19:02:43 -0700

    formatting back to python3

--- photon-lib/py/buildAndTest.sh
+++ photon-lib/py/buildAndTest.sh
@@ -1,13 +1,13 @@
 # Uninstall if it already was installed
-python -m pip uninstall -y photonlibpy
+python3 -m pip uninstall -y photonlibpy
 
 # Build wheel
-python setup.py bdist_wheel
+python3 setup.py bdist_wheel
 
 # Install whatever wheel was made
 for f in dist/*.whl; do
     echo "installing $f"
-    python -m pip install --no-cache-dir "$f"
+    python3 -m pip install --no-cache-dir "$f"
 done
 
 # Run the test suite
