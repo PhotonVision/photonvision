60470f9384da0735759f8a4ed98d6df7d748b7b8
Yishai Levy <96019039+levyishai@users.noreply.github.com>
2025-02-07 01:30:39 +0200

    Remove whitespace

--- photon-core/src/main/java/org/photonvision/vision/pipeline/ObjectDetectionPipeline.java
+++ photon-core/src/main/java/org/photonvision/vision/pipeline/ObjectDetectionPipeline.java
@@ -73,7 +73,6 @@ public class ObjectDetectionPipeline
         }
 
         params.model = selectedModel.get();
-
         params.useAllCores = settings.useAllCores;
 
         objectDetectorPipe.setParams(params);
