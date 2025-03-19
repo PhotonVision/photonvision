efe85d7a41059b615faa00ed002da695bab33418
samfreund <techguy763@gmail.com>
2025-03-18 23:31:11 -0500

    back to where it was originally

--- photon-lib/py/photonlibpy/simulation/photonCameraSim.py
+++ photon-lib/py/photonlibpy/simulation/photonCameraSim.py
@@ -476,11 +476,11 @@ class PhotonCameraSim:
             )
 
         intrinsics = self.prop.getIntrinsics()
-        intrinsicsView = typing.List[intrinsics.flatten().tolist()]
+        intrinsicsView = intrinsics.flatten().tolist()
         self.ts.cameraIntrinsicsPublisher.set(intrinsicsView, receiveTimestamp_us)
 
         distortion = self.prop.getDistCoeffs()
-        distortionView = typing.List[distortion.flatten().tolist()]
+        distortionView = distortion.flatten().tolist()
         self.ts.cameraDistortionPublisher.set(distortionView, receiveTimestamp_us)
 
         self.ts.heartbeatPublisher.set(self.heartbeatCounter, receiveTimestamp_us)
