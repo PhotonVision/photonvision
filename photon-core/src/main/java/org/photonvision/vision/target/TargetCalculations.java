67f66cd361e2a8b1f660efcf673fc1b48d59ca62
Gold856 <117957790+Gold856@users.noreply.github.com>
2025-04-14 19:37:53 -0400

    Clean up .gitignore

--- photon-core/src/main/java/org/photonvision/vision/target/TargetCalculations.java
+++ photon-core/src/main/java/org/photonvision/vision/target/TargetCalculations.java
@@ -14,6 +14,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
+
 package org.photonvision.vision.target;
 
 import org.opencv.calib3d.Calib3d;
@@ -27,7 +28,6 @@ import org.photonvision.vision.calibration.CameraCalibrationCoefficients;
 import org.photonvision.vision.opencv.DualOffsetValues;
 
 public class TargetCalculations {
-
     /**
      * Calculates the yaw and pitch of a point in the image. Yaw and pitch must be calculated together
      * to account for perspective distortion. Yaw is positive right, and pitch is positive up.
@@ -49,7 +49,6 @@ public class TargetCalculations {
             double targetCenterY,
             double verticalFocalLength,
             CameraCalibrationCoefficients cameraCal) {
-
         if (cameraCal != null) {
             // undistort
             MatOfPoint2f temp = new MatOfPoint2f(new Point(targetCenterX, targetCenterY));
