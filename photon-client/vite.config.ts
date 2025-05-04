99a0a5c64e2213ebec5779f48db7c8e89d4f09f3
Gold856 <117957790+Gold856@users.noreply.github.com>
2025-05-04 18:58:10 -0400

    Try overriding vuetify's styling

--- photon-client/vite.config.ts
+++ photon-client/vite.config.ts
@@ -1,26 +1,18 @@
 import { fileURLToPath, URL } from "node:url";
 
 import { defineConfig } from "vite";
-import Vue from "@vitejs/plugin-vue";
-import Components from "unplugin-vue-components/vite";
-import { Vuetify3Resolver } from "unplugin-vue-components/resolvers";
+import vue from "@vitejs/plugin-vue";
+import vuetify from "vite-plugin-vuetify";
 
 export default defineConfig({
   base: "./",
   plugins: [
-    Vue(),
-    Components({
-      resolvers: [Vuetify3Resolver()],
-      dts: true,
-      transformer: "vue3",
-      types: [
-        {
-          from: "vue-router",
-          names: ["RouterLink", "RouterView"]
-        }
-      ],
-      version: 2.7
-    })
+    vue(),
+    vuetify({
+      styles: {
+        configFile: 'src/assets/styles/settings.scss',
+      },
+    }),
   ],
   css: {
     preprocessorOptions: {
