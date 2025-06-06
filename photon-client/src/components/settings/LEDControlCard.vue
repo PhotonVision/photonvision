eac46fb85351563511165cac47323dd65309b18c
Gold856 <117957790+Gold856@users.noreply.github.com>
2025-06-06 10:54:49 -0400

    Fix LED control card padding

--- photon-client/src/components/settings/LEDControlCard.vue
+++ photon-client/src/components/settings/LEDControlCard.vue
@@ -4,18 +4,15 @@ import { useSettingsStore } from "@/stores/settings/GeneralSettingsStore";
 </script>
 
 <template>
-  <v-card class="mb-3 pr-6 pb-3" style="background-color: #006492">
-    <v-card-title>LED Control</v-card-title>
-    <div class="ml-5">
+  <v-card class="mb-3" style="background-color: #006492">
+      <v-card-title class="pa-6 pb-3">LED Control</v-card-title>
       <pv-slider
-        v-model="useSettingsStore().lighting.brightness"
-        label="Brightness"
-        class="pt-2"
-        :slider-cols="12"
-        :min="0"
-        :max="100"
-        @update:modelValue="(args) => useSettingsStore().changeLEDBrightness(args)"
+      v-model="useSettingsStore().lighting.brightness"
+      label="Brightness"
+      class="pa-6 pt-0"
+      :min="0"
+      :max="100"
+      @update:modelValue="(args) => useSettingsStore().changeLEDBrightness(args)"
       />
-    </div>
   </v-card>
 </template>
