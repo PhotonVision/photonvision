7def49ae6cde902824f630b9aa062b283f5a38db
Jade Turner <spacey-sooty@proton.me>
2025-06-24 10:17:44 +0800

    Start on updating examples for 2027
    Signed-off-by: Jade Turner <spacey-sooty@proton.me>

--- photonlib-java-examples/aimattarget/src/test/java/frc/robot/JniLoadTest.java
+++ photonlib-java-examples/aimattarget/src/test/java/frc/robot/JniLoadTest.java
@@ -24,12 +24,12 @@
 
 package frc.robot;
 
-import static org.junit.Assert.fail;
+import static org.junit.jupiter.api.Assertions.fail;
 
 import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import java.util.List;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.photonvision.PhotonCamera;
 import org.photonvision.simulation.PhotonCameraSim;
 import org.photonvision.simulation.SimCameraProperties;
