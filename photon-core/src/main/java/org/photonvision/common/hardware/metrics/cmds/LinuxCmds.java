433e1a53d98e1dc34e6a4ca11ea07557301e0303
samfreund <techguy763@gmail.com>
2025-07-04 08:27:21 -0500

    strip percent from cpu util

--- photon-core/src/main/java/org/photonvision/common/hardware/metrics/cmds/LinuxCmds.java
+++ photon-core/src/main/java/org/photonvision/common/hardware/metrics/cmds/LinuxCmds.java
@@ -27,7 +27,7 @@ public class LinuxCmds extends CmdBase {
         // TODO: boards have lots of thermal devices. Hard to pick the CPU
 
         cpuUtilizationCommand =
-                "top -bn1 | grep \"Cpu(s)\" | sed \"s/.*, *\\([0-9.]*\\)%* id.*/\\1/\" | awk '{print 100 - $1}'";
+                "top -bn1 | grep \"Cpu(s)\" | sed \"s/.*, *\\([0-9.]*\\)%* id.*/\\1/\" | awk '{print 100 - $1}' | cut -d'%' -f1";
 
         cpuUptimeCommand = "awk '{print $1}' /proc/uptime";
 
