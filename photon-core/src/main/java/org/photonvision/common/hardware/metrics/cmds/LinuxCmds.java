93d147113bd6ed975b29e9c93ab91c7b26154d64
samfreund <techguy763@gmail.com>
2025-07-04 08:24:55 -0500

    update cpu uptime and disk usage command

--- photon-core/src/main/java/org/photonvision/common/hardware/metrics/cmds/LinuxCmds.java
+++ photon-core/src/main/java/org/photonvision/common/hardware/metrics/cmds/LinuxCmds.java
@@ -29,12 +29,12 @@ public class LinuxCmds extends CmdBase {
         cpuUtilizationCommand =
                 "top -bn1 | grep \"Cpu(s)\" | sed \"s/.*, *\\([0-9.]*\\)%* id.*/\\1/\" | awk '{print 100 - $1}'";
 
-        cpuUptimeCommand = "uptime -p | cut -c 4-";
+        cpuUptimeCommand = "awk '{print $1}' /proc/uptime";
 
         // RAM
         ramUsageCommand = "free -m | awk 'FNR == 2 {print $3}'";
 
         // Disk
-        diskUsageCommand = "df ./ --output=pcent | tail -n +2";
+        diskUsageCommand = "df ./ --output=pcent | tail -n +2 | cut -d'%' -f1";
     }
 }
