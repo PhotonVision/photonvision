be07e8f8c9edaeec95c2efa515beb09378e8ce40
samfreund <samf.236@proton.me>
2025-10-23 01:04:00 -0500

    add axios post util

--- photon-client/src/lib/PhotonUtils.ts
+++ photon-client/src/lib/PhotonUtils.ts
@@ -1,4 +1,6 @@
+import { useStateStore } from "@/stores/StateStore";
 import type { Resolution } from "@/types/SettingTypes";
+import axios from "axios";
 
 export const resolutionsAreEqual = (a: Resolution, b: Resolution) => {
   return a.height === b.height && a.width === b.width;
@@ -17,4 +19,33 @@ export const parseJsonFile = async <T extends Record<string, any>>(file: File): 
     fileReader.onerror = (error) => reject(error);
     fileReader.readAsText(file);
   });
+};
+
+export const axiosPost = (url: string, data?: any, config?: any): Promise<void> => {
+  return axios
+    .post(url, data, config)
+    .then(() => {
+      useStateStore().showSnackbarMessage({
+        message: "Successfully dispatched the command. Waiting for backend to respond",
+        color: "success"
+      });
+    })
+    .catch((error) => {
+      if (error.response) {
+        useStateStore().showSnackbarMessage({
+          message: "The backend is unable to fulfill the request.",
+          color: "error"
+        });
+      } else if (error.request) {
+        useStateStore().showSnackbarMessage({
+          message: "Error while trying to process the request! The backend didn't respond.",
+          color: "error"
+        });
+      } else {
+        useStateStore().showSnackbarMessage({
+          message: "An error occurred while trying to process the request.",
+          color: "error"
+        });
+      }
+    });
 };
