84151674ada6e3b865e494e122bb24b74384d17f
Gold856 <117957790+Gold856@users.noreply.github.com>
2025-11-01 23:48:28 -0400

    Remove unnecessary v-if

--- photon-client/src/views/DashboardView.vue
+++ photon-client/src/views/DashboardView.vue
@@ -146,12 +146,7 @@ const showCameraSetupDialog = ref(useCameraSettingsStore().needsCameraConfigurat
     <PipelineConfigCard />
 
     <!-- TODO - not sure this belongs here -->
-    <v-dialog
-      v-if="useCameraSettingsStore().needsCameraConfiguration"
-      v-model="showCameraSetupDialog"
-      max-width="800"
-      dark
-    >
+    <v-dialog v-model="showCameraSetupDialog" max-width="800" dark>
       <v-card flat color="surface">
         <v-card-title>Set up some cameras to get started!</v-card-title>
         <v-card-text class="pt-0">
