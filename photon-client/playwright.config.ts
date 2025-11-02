ecb0948400e1ac040af62abf691283447dc0fa95
samfreund <samf.236@proton.me>
2025-11-02 01:14:05 -0600

    make webserver OS agnostic

--- photon-client/playwright.config.ts
+++ photon-client/playwright.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig, devices } from "@playwright/test";
+import path from "path";
 
 /**
  * Read environment variables from file.
@@ -72,7 +73,7 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: "./gradlew run",
+    command: path.normalize("./gradlew") + " run",
     url: "http://localhost:5800",
     timeout: 300 * 1000,
     reuseExistingServer: !process.env.CI,
