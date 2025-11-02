2dc1fe22ac1057a208ac3d6bf9723c26e088c16e
samfreund <samf.236@proton.me>
2025-11-02 14:34:41 -0600

    opi and rubik OD checks

--- photon-client/tests/platformDependent/rubik.spec.ts
+++ photon-client/tests/platformDependent/rubik.spec.ts
@@ -1,13 +1,70 @@
 import { expect } from "@playwright/test";
 import { test } from "../fixtures";
 import axios from "axios";
+import path from "path";
+
+const fakeModelName = "FAKE-MODEL";
+const fakeLabels = "test, 1, woof";
+const newModelName = "foo-bar";
 
 test.beforeEach(async ({ page }) => {
   page.goto("/#/settings");
   await axios.post("/override/platform", { platform: "rubik" });
+  await axios.post("/activateTestMode");
 });
 
 test("testSettingsPage", async ({ page }) => {
   await expect(page.getByRole("main")).toContainText("Linux AARCH 64-bit with QCS6490");
   await expect(page.getByText("Object Detection")).toBeVisible();
+});
+
+test("Upload model", async ({ page }) => {
+  const testsDir = process.env.TESTS_DIR;
+  if (!testsDir) {
+    throw new Error("TESTS_DIR is not set");
+  }
+
+  await page.getByRole("button", { name: "Import Model" }).click();
+  await page.getByRole("textbox", { name: "Labels" }).fill(fakeLabels);
+  await page.getByRole("spinbutton", { name: "Width" }).fill("640");
+  await page.getByRole("spinbutton", { name: "Height" }).fill("640");
+  await page.getByTestId("import-version-select").click();
+  await page.getByRole("option", { name: "YOLOv8" }).click();
+  await page
+    .getByRole("button", { name: "Model File Model File" })
+    .setInputFiles(path.normalize(testsDir + "/tests/resources/" + fakeModelName + ".tflite"));
+  await page.getByRole("button", { name: "Import Object Detection Model" }).click();
+
+  await page.goto("/#/settings");
+  // TODO: replace FAKE-MODE with FAKE-MODEL once model import bugfix is merged
+  const tableRow = page.getByTestId("model-table").locator("tr", { hasText: fakeModelName });
+
+  await expect(tableRow).toBeVisible();
+  await expect(tableRow).toContainText(fakeLabels);
+});
+
+test("Rename model", async ({ page }) => {
+  // TODO: replace FAKE-MODE with FAKE-MODEL once model import bugfix is merged
+  const tableRow = page.getByTestId("model-table").locator("tr", { hasText: fakeModelName });
+
+  await tableRow.getByRole("button", { name: "Rename Model" }).click();
+  await page.getByRole("textbox", { name: "New Name New Name" }).fill(newModelName);
+  await page.getByRole("button", { name: "Rename", exact: true }).click();
+
+  await page.reload();
+
+  const renamedRow = page.getByTestId("model-table").locator("tr", { hasText: newModelName });
+
+  await expect(renamedRow).toContainText(fakeLabels);
+});
+
+test("Delete model", async ({ page }) => {
+  const tableRow = page.getByTestId("model-table").locator("tr", { hasText: newModelName });
+
+  await tableRow.getByRole("button", { name: "Delete Model" }).click();
+  await page.getByRole("button", { name: "Delete", exact: true }).click();
+
+  await page.reload();
+  const deletedRow = page.getByTestId("model-table").locator("tr", { hasText: newModelName });
+  await expect(deletedRow).toHaveCount(0);
 });
