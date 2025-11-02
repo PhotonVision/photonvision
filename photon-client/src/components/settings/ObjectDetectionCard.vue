806b6ff387e43f0a8f56e9bb30502af63e947f06
samfreund <samf.236@proton.me>
2025-11-02 14:32:56 -0600

    add ids for testing

--- photon-client/src/components/settings/ObjectDetectionCard.vue
+++ photon-client/src/components/settings/ObjectDetectionCard.vue
@@ -235,6 +235,7 @@ const handleBulkImport = () => {
                     v-model="importVersion"
                     variant="underlined"
                     label="Model Version"
+                    data-testid="import-version-select"
                     :items="
                       useSettingsStore().general.supportedBackends?.includes('RKNN')
                         ? ['YOLOv5', 'YOLOv8', 'YOLO11']
@@ -335,7 +336,7 @@ const handleBulkImport = () => {
                 <th>Info</th>
               </tr>
             </thead>
-            <tbody>
+            <tbody data-testid="model-table">
               <tr v-for="model in supportedModels" :key="model.modelPath">
                 <td>{{ model.nickname }}</td>
                 <td>{{ model.labels.join(", ") }}</td>
