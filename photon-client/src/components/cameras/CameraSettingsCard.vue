315fc49a7bfe71e8a6176bfd55cd3fefbbe248eb
ElectricTurtle32 <49703859+ElectricTurtle32@users.noreply.github.com>
2025-11-11 12:51:41 -0600

    fix a few things

--- photon-client/src/components/cameras/CameraSettingsCard.vue
+++ photon-client/src/components/cameras/CameraSettingsCard.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
 import PvSelect, { type SelectItem } from "@/components/common/pv-select.vue";
+import PvSwitch from "@/components/common/pv-switch.vue";
 import PvInput from "@/components/common/pv-input.vue";
 import PvNumberInput from "@/components/common/pv-number-input.vue";
+
 import { useCameraSettingsStore } from "@/stores/settings/CameraSettingsStore";
 import { useStateStore } from "@/stores/StateStore";
 import { computed, ref, watchEffect } from "vue";
@@ -142,8 +144,8 @@ const wrappedCameras = computed<SelectItem[]>(() =>
 
 <template>
   <v-card class="mb-3 rounded-12" color="surface" dark>
-    <v-card-title class="pb-1">Camera Settings</v-card-title>
-    <v-card-text class="pb-0">
+    <v-card-title class="pb-0">Camera Settings</v-card-title>
+    <v-card-text class="pb-3">
       <pv-select
         v-model="useStateStore().currentCameraUniqueName"
         label="Camera"
@@ -173,35 +175,25 @@ const wrappedCameras = computed<SelectItem[]>(() =>
         ]"
         :select-cols="8"
       />
+     <v-row class="mb-3" >
+      <pv-switch
+           
+              v-model="focusMode"
+              label="Focus Mode"
+              tooltip="Turns of focus mode"
+              :label-cols="8"
+            />
+             <v-card-text class="pt-5 ">
+              Focus: {{ Math.round(useStateStore().currentPipelineResults?.focus || 0) }}
+  </v-card-text>
+         </v-row>
+
     </v-card-text>
-    <v-card-text class="pt-0">
-      <v-row align="center" no-gutters style="min-height: 40px">
-        <v-col cols="4" class="pa-0 pr-2 d-flex align-center" style="height: 40px">
-          <span style="min-width: 90px; text-align: left; line-height: 40px">Focus Mode</span>
-        </v-col>
-        <v-col cols="3" class="pa-2 d-flex align-center" style="height: 40px">
-          <v-switch
-            v-model="focusMode"
-            :disabled="
-              useCameraSettingsStore().isCalibrationMode || useCameraSettingsStore().pipelineNames.length === 0
-            "
-            hide-details="auto"
-            color="primary"
-            style="margin-right: 0px; margin-top: 0; margin-bottom: 0"
-          />
-        </v-col>
-        <v-col cols="5" class="pa-0 pr-5 d-flex align-center justify-end" style="height: 40px">
-          <div
-            v-if="useStateStore().currentPipelineResults?.focus !== undefined"
-            style="display: flex; align-items: center; gap: 8px; white-space: nowrap; height: 40px"
-          >
-            <span class="pr-1" style="display: inline-block; min-width: 80px; text-align: right; line-height: 40px"
-              >Focus: {{ Math.round(useStateStore().currentPipelineResults?.focus || 0) }}</span
-            >
-          </div>
-        </v-col>
-      </v-row>
-    </v-card-text>
+    
+    
+      
+        
+     
     <v-card-text class="d-flex pt-0">
       <v-col cols="6" class="pa-0 pr-2">
         <v-btn
