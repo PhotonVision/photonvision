a7329c48a31524944ec8c135fa118b1204ed6f82
samfreund <samf.236@proton.me>
2025-11-25 15:31:11 -0600

    lint

--- photon-lib/py/docs/source/conf.py
+++ photon-lib/py/docs/source/conf.py
@@ -2,7 +2,7 @@ import os
 import sys
 
 # This adds the 'py/' directory to the Python path
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..")))
 
 # Configuration file for the Sphinx documentation builder.
 #
