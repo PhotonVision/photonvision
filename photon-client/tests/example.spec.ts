261aa903d687e57164d38478f0bddfe2c3cbee9e
samfreund <samf.236@proton.me>
2025-12-04 11:39:30 -0600

    remove example test

--- photon-client/tests/example.spec.ts
+++
@@ -1,9 +0,0 @@
-import { expect } from "@playwright/test";
-import { test } from "./fixtures";
-
-test("has title", async ({ page }) => {
-  await page.goto("");
-
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Photon Client/);
-});
