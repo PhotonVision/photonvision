d30ae6cc27376b2763935e78b5f0f678bd03efe7
Graham <grahamshllk@gmail.com>
2025-12-30 01:44:08 -0500

    Refactor website (#2243)
    ## Description
    
    Switches to Vue for easier maintenance (pre-built in CI). Changes UI to
    look a little nicer
    <img width="1920" height="1080" alt="image"
    src="https://github.com/user-attachments/assets/c5933326-c391-4d5c-8b3c-d3eeaa11a2f9"
    />
    <img width="1920" height="1080" alt="image"
    src="https://github.com/user-attachments/assets/d933c8d0-e5a0-40c0-bc80-0c4c8a4cc4f0"
    />
    <img width="1920" height="1080" alt="image"
    src="https://github.com/user-attachments/assets/3d8a652b-bd49-4147-a269-140e79fd4164"
    />
    <img width="1021" height="1352" alt="image"
    src="https://github.com/user-attachments/assets/545a9c02-541c-4a34-b81f-587f21bc682f"
    />
    <img width="957" height="1196" alt="image"
    src="https://github.com/user-attachments/assets/0dfd8080-0ffe-48c5-8fe0-11177f95dfc9"
    />
    
    
    ## Meta
    
    Merge checklist:
    - [x] Pull Request title is [short, imperative
    summary](https://cbea.ms/git-commit/) of proposed changes
    - [x] The description documents the _what_ and _why_
    - [ ] If this PR changes behavior or adds a feature, user documentation
    is updated
    - [ ] If this PR touches photon-serde, all messages have been
    regenerated and hashes have not changed unexpectedly
    - [ ] If this PR touches configuration, this is backwards compatible
    with settings back to v2025.3.2
    - [ ] If this PR touches pipeline settings or anything related to data
    exchange, the frontend typing is updated
    - [ ] If this PR addresses a bug, a regression test for it is added
    
    ---------
    
    Co-authored-by: Gold856 <117957790+Gold856@users.noreply.github.com>

--- website/vite.config.ts
+++ website/vite.config.ts
@@ -1,5 +1,7 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
+import vue from "@vitejs/plugin-vue";
+
 export default defineConfig({
-  plugins: [tailwindcss()],
+  plugins: [vue(), tailwindcss()],
 });
