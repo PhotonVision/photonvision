721cda8cdeff0c7064bc2d814d09df8acb2f173e
samfreund <samf.236@proton.me>
2026-01-11 23:42:41 -0600

    test something else

--- photon-lib/py/setup.py
+++ photon-lib/py/setup.py
@@ -60,7 +60,7 @@ setup(
     package_data={"photonlibpy": ["py.typed"]},
     version=versionString,
     install_requires=[
-        "numpy~=2.2",
+        "numpy~=2.4",
         "wpilib==2026.1.1",
         "robotpy-wpimath==2026.1.1",
         "robotpy-apriltag==2026.1.1",
