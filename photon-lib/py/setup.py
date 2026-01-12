64a4fdc2ef32bdad9a593bdea3a17199d75c9539
Matt Morley <matthew.morley.ca@gmail.com>
2026-01-11 21:14:51 -0800

    Update numpy version requirement to 2.2

--- photon-lib/py/setup.py
+++ photon-lib/py/setup.py
@@ -60,7 +60,7 @@ setup(
     package_data={"photonlibpy": ["py.typed"]},
     version=versionString,
     install_requires=[
-        "numpy~=2.1",
+        "numpy~=2.2",
         "wpilib==2026.1.1",
         "robotpy-wpimath==2026.1.1",
         "robotpy-apriltag==2026.1.1",
