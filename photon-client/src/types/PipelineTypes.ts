a85ded9d0a2dcb5ae02bcdd8bcd89b8a703c88f8
John Fogarty <john.fogarty@aol.com>
2026-01-20 18:21:17 -0800

    testing enhancements to ROI size fallback

--- photon-client/src/types/PipelineTypes.ts
+++ photon-client/src/types/PipelineTypes.ts
@@ -235,6 +235,7 @@ export interface AprilTagPipelineSettings extends PipelineSettings {
   mlRoiExpansionFactor: number;
   mlFallbackToTraditional: boolean;
   mlModelName: string | null;
+  mlLargeRoiFallbackRatio: number;
 }
 export type ConfigurableAprilTagPipelineSettings = Partial<
   Omit<AprilTagPipelineSettings, "pipelineType" | "hammingDist" | "debug">
@@ -265,7 +266,8 @@ export const DefaultAprilTagPipelineSettings: AprilTagPipelineSettings = {
   mlNmsThreshold: 0.45,
   mlRoiExpansionFactor: 1.2,
   mlFallbackToTraditional: true,
-  mlModelName: null
+  mlModelName: null,
+  mlLargeRoiFallbackRatio: 0.25
 };
 
 export interface ArucoPipelineSettings extends PipelineSettings {
