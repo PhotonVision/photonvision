a989a9e1a8b1376f9dcab31141caac349bc7854a
Gold856 <117957790+Gold856@users.noreply.github.com>
2026-01-25 15:13:06 -0500

    Reorder and add long sleeps

--- photon-core/src/main/java/org/photonvision/vision/camera/USBCameras/GenericUSBCameraSettables.java
+++ photon-core/src/main/java/org/photonvision/vision/camera/USBCameras/GenericUSBCameraSettables.java
@@ -173,8 +173,15 @@ public class GenericUSBCameraSettables extends VisionSourceSettables {
                 && !cameraAutoExposure) {
             // OV9281 on Linux seems to sometimes ignore our exposure requests on first boot if we're in
             // manual mode. Poking the camera into and out of auto exposure seems to fix it.
-            setAutoExposureImpl(true);
-            setAutoExposureImpl(false);
+            try {
+                setAutoExposureImpl(false);
+                Thread.sleep(2000);
+                setAutoExposureImpl(true);
+                Thread.sleep(2000);
+                setAutoExposureImpl(false);
+            } catch (InterruptedException e) {
+                logger.error("Thread interrupted while setting OV9281 exposure!", e);
+            }
         } else {
             setAutoExposureImpl(cameraAutoExposure);
         }
