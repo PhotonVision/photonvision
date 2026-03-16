9e3e695896885b9c06976b1fe2e6de02c3abf618
Matt Morley <matthew.morley.ca@gmail.com>
2026-03-15 19:06:36 -0700

    stuff and things

--- photon-core/src/main/java/org/photonvision/common/networking/RoborioFinder.java
+++ photon-core/src/main/java/org/photonvision/common/networking/RoborioFinder.java
@@ -16,19 +16,32 @@
  */
 package org.photonvision.common.networking;
 
-import edu.wpi.first.util.MulticastServiceResolver;
-import edu.wpi.first.util.ServiceData;
-import edu.wpi.first.util.WPIUtilJNI;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+
+import org.photonvision.common.logging.LogGroup;
+import org.photonvision.common.logging.Logger;
+
+import edu.wpi.first.net.MulticastServiceAnnouncer;
+import edu.wpi.first.net.MulticastServiceResolver;
+import edu.wpi.first.net.ServiceData;
+import edu.wpi.first.util.WPIUtilJNI;
 
 /**
  * Goal here is two-fold 1. Provide a way for us to discover roborios and show to users. this one is
  * of questionable importance, and requires we resolve _ni._tcp. I don't know how this will work
  * with SystemCore 2. Respond to any multicast service requests emitted by the roborio/systemcore.
  * Our vendordep would be the thing sending those pokes out
+ * 
+ * Looks like systemcore advertises _ni._tcp as well as _SystemCore._tcp
  */
 public class RoborioFinder {
+    private static final Logger logger = new Logger(RoborioFinder.class, LogGroup.General);
+
     private static RoborioFinder INSTANCE;
 
     public static RoborioFinder getInstance() {
@@ -36,48 +49,47 @@ public class RoborioFinder {
         return INSTANCE;
     }
 
-    List<ServiceData> possibleRioList = new ArrayList<>();
+    // Map of ipv4 to possible robot controller
+    volatile HashMap<Long, ServiceData> possibleRioList = new HashMap<>();
 
     private final MulticastServiceResolver resolver = new MulticastServiceResolver("_ni._tcp");
+    private final MulticastServiceAnnouncer announcer = new MulticastServiceAnnouncer("photonvision",  "_photon", 0);
 
-    public List<ServiceData> findAll() {
-
-        if (!resolver.hasImplementation()) return possibleRioList;
+    public Collection<ServiceData> findAll() {
+        if (!resolver.hasImplementation()) return possibleRioList.values();
 
         var event = resolver.getEventHandle();
         try {
             var timedOut = WPIUtilJNI.waitForObjectTimeout(event, 0);
-            if (timedOut) return possibleRioList;
+            if (timedOut) return possibleRioList.values();
         } catch (InterruptedException e) {
             e.printStackTrace();
-            return possibleRioList;
+            return possibleRioList.values();
         }
 
         var allData = resolver.getData();
-        if (allData == null) return possibleRioList;
+        if (allData == null) return possibleRioList.values();
+
         for (var data : allData) {
-            // Don't add if it doesn't have the "MAC" key
-            if (!data.getTxt().containsKey("MAC")) {
-                continue;
-            }
-
-            // If we already see the ipv4, don't add it
-            if (possibleRioList.stream().anyMatch(it -> it.getIpv4Address() == data.getIpv4Address())) {
-                continue;
-            }
-
-            possibleRioList.add(data);
+            possibleRioList.put(data.getIpv4Address(), data);
         }
 
-        possibleRioList.forEach(it -> System.out.println(it.getHostName()));
-        return possibleRioList;
+        possibleRioList.values().forEach(it -> System.out.println(it.getHostName()));
+
+        return possibleRioList.values();
     }
 
     public void start() {
-        resolver.start();
+        if (resolver.hasImplementation() && announcer.hasImplementation()) {
+            resolver.start();
+            announcer.start();
+        } else {
+            logger.error("No implementation");
+        }
     }
 
     public void stop() {
         resolver.stop();
+        announcer.stop();
     }
 }
--- photon-core/src/test/java/org/photonvision/common/networking/FakeRioAnnouncer.java
+++ photon-core/src/test/java/org/photonvision/common/networking/FakeRioAnnouncer.java
@@ -1,9 +1,12 @@
 package org.photonvision.common.networking;
 
-import edu.wpi.first.util.MulticastServiceAnnouncer;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import java.util.HashMap;
 import java.util.Map;
 import org.junit.jupiter.api.Test;
+
+import edu.wpi.first.net.MulticastServiceAnnouncer;
 
 public class FakeRioAnnouncer {
 
@@ -12,6 +15,7 @@ public class FakeRioAnnouncer {
         Map<String, String> text = new HashMap<>();
         text.put("MAC", "fooabr");
         var a = new MulticastServiceAnnouncer("foo", "_ni._tcp", 19213, text);
+        assertTrue(a.hasImplementation());
         a.start();
         while (!Thread.currentThread().isInterrupted()) {
             try {
