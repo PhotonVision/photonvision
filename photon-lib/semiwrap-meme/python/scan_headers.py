6faa42fb6775428edb96b4306d28f08dd3287801
Jesse Kane <jessekane66@gmail.com>
2026-05-06 14:43:13 -0400

    got semiwrap to start generating actual yaml files

---
+++ photon-lib/semiwrap-meme/python/scan_headers.py
@@ -0,0 +1,24 @@
+import os
+
+from pathlib import Path
+
+sw_root_dir = Path(__file__).resolve().parent.parent
+
+pkgconfig_dir = sw_root_dir / "native" / "src" / "photonlib-native"
+
+existing = os.environ.get("PKG_CONFIG_PATH", "")
+
+os.environ["PKG_CONFIG_PATH"] = (
+    f"{str(pkgconfig_dir)}:{existing}"
+    if existing
+    else str(pkgconfig_dir)
+)
+
+import subprocess
+
+subprocess.call([
+    "python",
+    "-m",
+    "semiwrap",
+    "scan-headers"
+])
