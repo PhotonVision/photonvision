5101cbeb80e76f78578a7192ba46f3eefd7c8f8b
Matt Morley <matthew.morley.ca@gmail.com>
2026-05-24 14:29:05 -0700

    Enforce minimum

--- photon-core/src/main/java/org/photonvision/vision/pipe/impl/Calibrate3dPipe.java
+++ photon-core/src/main/java/org/photonvision/vision/pipe/impl/Calibrate3dPipe.java
@@ -97,6 +97,12 @@ public class Calibrate3dPipe
             return null;
         }
 
+        // Make sure we have something
+        if (filteredIn.isEmpty()) {
+            logger.error("I got zero observations?");
+            return null;
+        }
+
         CameraCalibrationCoefficients ret;
         var start = System.nanoTime();
 
