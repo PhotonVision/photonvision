34d24b64201cf73b8aa55d0d0bb9626dc0703b0a
Jesse Kane <91845950+Bobcat66@users.noreply.github.com>
2026-07-29 00:35:01 -0400

    Update photon-targeting/src/main/java/org/photonvision/common/dataflow/structures/Packet.java
    Co-authored-by: Matt Morley <matthew.morley.ca@gmail.com>

--- photon-targeting/src/main/java/org/photonvision/common/dataflow/structures/Packet.java
+++ photon-targeting/src/main/java/org/photonvision/common/dataflow/structures/Packet.java
@@ -276,7 +276,7 @@ public class Packet {
     public <T extends PhotonStructSerializable<T>> void encodeList(List<T> data) {
         // Hack
         BiConsumer<Packet, T> encoder;
-        if (data.size() < 1) {
+        if (data.isEmpty()) {
             encoder =
                     (packet, value) -> {
                         ;
