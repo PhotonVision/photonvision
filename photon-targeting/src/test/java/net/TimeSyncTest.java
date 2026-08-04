5dc827094782a52e3511c8da6189999da0a058d5
samfreund <samf.236@proton.me>
2026-08-03 18:52:27 -0700

    remove redundant timeout
    Signed-off-by: samfreund <samf.236@proton.me>

--- photon-targeting/src/test/java/net/TimeSyncTest.java
+++ photon-targeting/src/test/java/net/TimeSyncTest.java
@@ -22,7 +22,6 @@ import java.util.concurrent.TimeUnit;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.Timeout;
 import org.photonvision.jni.LibraryLoader;
 import org.photonvision.jni.TimeSyncClient;
 import org.photonvision.jni.TimeSyncServer;
@@ -44,7 +43,6 @@ public class TimeSyncTest {
     }
 
     @Test
-    @Timeout(value = 10, unit = TimeUnit.MINUTES)
     public void smoketest() throws InterruptedException {
         // NetworkTableInstance.getDefault().stopClient();
         // NetworkTableInstance.getDefault().startServer();
