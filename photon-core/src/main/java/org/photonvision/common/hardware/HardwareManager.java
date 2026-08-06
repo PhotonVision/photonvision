55d8b32734a95d19ba89c9124879ca02e2b927d5
Jesse Kane <jessekane66@gmail.com>
2026-08-05 22:15:29 -0400

    deleted extra space

--- photon-core/src/main/java/org/photonvision/common/hardware/HardwareManager.java
+++ photon-core/src/main/java/org/photonvision/common/hardware/HardwareManager.java
@@ -58,7 +58,7 @@ public class HardwareManager {
 
     private Optional<NTDataChangeListener> ledModeListener;
 
-    public  Optional<VisionLED> visionLED;
+    public Optional<VisionLED> visionLED;
 
     public static HardwareManager getInstance() {
         if (instance == null) {
