06e0845efc4e70a7dfe4dcc0f3bd11e76d2c2de3
thatcomputerguy0101 <thatcomputerguy0101@gmail.com>
2026-08-12 23:59:48 -0400

    Enable ipv6 link local address to keep a stable ipv4 link local address

--- photon-core/src/main/java/org/photonvision/common/networking/NetworkingCommands.java
+++ photon-core/src/main/java/org/photonvision/common/networking/NetworkingCommands.java
@@ -40,7 +40,7 @@ public class NetworkingCommands {
         nmcli connection modify "${connection}"
         autoconnect yes
         ipv4.method auto
-        ipv6.method disabled
+        ipv6.method link-local
         ipv4.link-local fallback
         """.replaceAll("[\\n]", " ");
 }
