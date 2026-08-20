11b7a46fc44a6d5971adf183244e87d2d49c61fc
Jesse Kane <jessekane66@gmail.com>
2026-08-20 12:41:18 -0400

    chnged getter to direct field access

--- photon-core/src/main/java/org/photonvision/vision/processes/VisionModule.java
+++ photon-core/src/main/java/org/photonvision/vision/processes/VisionModule.java
@@ -183,8 +183,8 @@ public class VisionModule implements AutoCloseable {
             visionSource.getSettables().setFOV(fov);
         }
 
-        // Configure LED's if supported by the underlying hardware
-        if (HardwareManager.getInstance().getVisionLED() != null && this.camShouldControlLEDs()) {
+        // Configure LED's if supported by the underlying hardware. TODO: Is a null check necessary here?
+        if (HardwareManager.getInstance().visionLED != null && this.camShouldControlLEDs()) {
             HardwareManager.getInstance()
                     .visionLED
                     .ifPresent(
