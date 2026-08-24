cc1da787fd06a26682a2588d535d1fa84211d2e0
Craig Schardt <crschardt@fastem.com>
2026-08-23 21:02:17 -0500

    remove unused imports and wpiformat

--- photon-core/src/main/java/org/photonvision/common/configuration/migrations/MigrationSteps.java
+++ photon-core/src/main/java/org/photonvision/common/configuration/migrations/MigrationSteps.java
@@ -1,8 +1,23 @@
+/*
+ * Copyright (C) Photon Vision.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package org.photonvision.common.configuration.migrations;
 
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -15,7 +30,7 @@ import org.photonvision.common.logging.Logger;
 import io.avaje.json.JsonException;
 
 public class MigrationSteps {
-    
+
     private static final Logger logger = new Logger(MigrationSteps.class, LogGroup.Config);
 
     public static LinkedMigrationStep buildMigrations() {
@@ -172,5 +187,5 @@ public class MigrationSteps {
             logger.error("Exception thrown during step", e);
         }
         return true;
-    };    
+    };
 }
