c45f86dee103ff28e24ee14cac1f1300293f38b9
John Fogarty <john.fogarty@aol.com>
2026-09-16 17:48:08 -0700

    prettier

--- photon-client/src/components/dashboard/tabs/InputTab.vue
+++ photon-client/src/components/dashboard/tabs/InputTab.vue
@@ -306,9 +306,7 @@ const interactiveCols = computed(() =>
     />
     <pv-switch
       v-model="useCameraSettingsStore().currentPipelineSettings.blockForFrames"
-      :disabled="
-        useCameraSettingsStore().currentCameraSettings.matchedCameraInfo.type !== PVUsbCamera
-      "
+      :disabled="useCameraSettingsStore().currentCameraSettings.matchedCameraInfo.type !== PVUsbCamera"
       label="Low Latency Mode"
       :switch-cols="interactiveCols"
       tooltip="When enabled, USB cameras wait for the next camera frame for lowest latency. When disabled, uses the most recent available frame for higher FPS."
